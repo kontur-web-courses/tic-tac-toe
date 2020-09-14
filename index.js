9ea9f1a28a6c36c58c4ada16c6866f581d77508a
whatisloveam <vladik2000@hotmail.com>
2020-09-14 20:36:07 +0500

    Крестики нолики, Мельников, Макаров

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
+const FIELD_SIZE = 3;
+const COUNT_CELLS = 9;
+const Xwon = "X won";
+const Owon = "0 won";
+const Draw = "Draw";
+const Continue = "Continue";
+const RED = "#ff0000";
+const BLACK = "#000000";
+
+let gameField = new Array(COUNT_CELLS);
+let numOfMoves = 0;
+let IsXMove = false; // false - 0, true - x
+let res = Continue;
+
+
+for (let i = 0; i < COUNT_CELLS; i++) 
+   gameField[i] = EMPTY;
+
+
+console.log(gameField);
 
 const container = document.getElementById('fieldWrapper');
 
@@ -29,11 +49,27 @@ function renderGrid (dimension) {
 function cellClickHandler (row, col) {
     // Пиши код тут
     console.log(`Clicked on cell: ${row}, ${col}`);
+    
+    if(gameField[row*3 + col] !== EMPTY || res === Draw || res === Xwon || res === Owon)
+        return;
 
+    IsXMove = !IsXMove;
+    gameField[row*3 + col] = IsXMove ? CROSS : ZERO;
+    renderSymbolInCell(IsXMove ? CROSS : ZERO, row, col); 
+    res = checkField();
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    if(res === Xwon) {
+        renderSymbolInCell
+        alert("The first player won!");
+        console.log(Xwon);
+
+    } else if (res === Owon) {
+        alert("The second player won!");
+        console.log(Owon);
+    } else if (res === Draw) {
+        alert("Победила дружба!");
+        console.log(Draw);
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -55,8 +91,87 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+    for (let i = 0; i < COUNT_CELLS; i++) {
+        renderSymbolInCell(EMPTY,i / 3 >> 0, i % 3, BLACK);
+        gameField[i] = EMPTY;
+    }
+    IsXMove = false;
+    res = Continue;
 }
 
+function checkField() {
+    for(let k = 0; k < COUNT_CELLS; k += FIELD_SIZE) {
+        if (gameField[k] === gameField[k + 1]
+         && gameField[k] === gameField[k + 2])
+        {
+            if (gameField[k] == CROSS) {
+                markWonStep(CROSS, k, k + 1, k + 2);
+                return Xwon;                
+            }
+            if (gameField[k] == ZERO) {
+                markWonStep(ZERO,  k, k + 1, k + 2);
+                return Owon;  
+            }
+        }  
+    }
+    for (let k = 0; k < COUNT_CELLS; k++)
+    {
+        if (gameField[k] === gameField[k + FIELD_SIZE]
+            && gameField[k] === gameField[k + FIELD_SIZE * 2])
+        {
+            
+            if (gameField[k] == CROSS) {
+                markWonStep(CROSS, k, k + FIELD_SIZE, k + FIELD_SIZE * 2);
+                return Xwon;
+            }
+            if (gameField[k] == ZERO) {
+                markWonStep(ZERO,  k, k + FIELD_SIZE, k + FIELD_SIZE * 2);
+                return Owon;
+            }
+                
+        }
+    }
+
+    if (gameField[0] == gameField[4] && gameField[8] == gameField[4] && gameField[8] !== EMPTY
+        || gameField[2] == gameField[4] && gameField[6] == gameField[4] && gameField[6] !== EMPTY)
+    {
+        if (gameField[0] == CROSS) {
+            markWonStep(CROSS,  0, 4, 8);
+            return Xwon;
+        }
+            
+        if (gameField[0] == ZERO) {
+            markWonStep(ZERO,  0, 4, 8);
+            return Owon;
+        }
+
+        if (gameField[2] == CROSS) {
+            markWonStep(CROSS,  2, 4, 6);
+            return Xwon;
+        }
+            
+        if (gameField[2] == ZERO) {
+            markWonStep(ZERO,  2, 4, 6);
+            return Owon;
+        }
+            
+    }
+
+    for (let i = 0; i < COUNT_CELLS; i++)
+    {
+        if (gameField[i] === EMPTY)
+            return Continue;
+    }
+    return Draw;
+}
+
+
+function markWonStep(winner, firstStep, secondStep, thirdStep)
+{
+      renderSymbolInCell(winner,firstStep / 3 >> 0, firstStep % 3, RED);
+      renderSymbolInCell(winner,secondStep / 3 >> 0, secondStep % 3, RED);
+      renderSymbolInCell(winner,thirdStep / 3 >> 0,thirdStep % 3, RED);    
+}
 
 /* Test Function */
 /* Победа первого игрока */
