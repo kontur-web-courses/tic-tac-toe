7a6edde95f4ed7a1a3a3685b5907d2cfba7182f5
Upgrade220 <kowalkof@yandex.ru>
2020-09-14 21:02:54 +0500

    final Result

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
 
+let field =[];
+var iteration = 1;
+
+let WIN_POSITIONS = [
+    'XXX      ',
+    '   XXX   ',
+    '      XXX',
+    'X  X  X  ',
+    ' X  X  X ',
+    '  X  X  X',
+    'X   X   X',
+    '  X X X  '
+  ];
+
 const container = document.getElementById('fieldWrapper');
 
 startGame();
@@ -9,6 +23,17 @@ addResetListener();
 
 function startGame () {
     renderGrid(3);
+    initialiseField();
+    iteration = 1;
+}
+
+function initialiseField () {
+    for(let i = 0; i<3 ; i++)
+    {
+        field[i]=[];
+        for(let j = 0; j<3 ; j++)
+            field[i][j]=EMPTY;
+    }
 }
 
 function renderGrid (dimension) {
@@ -28,12 +53,52 @@ function renderGrid (dimension) {
 
 function cellClickHandler (row, col) {
     // Пиши код тут
+
+    if(iteration === 9) alert('Победила дружба');
+    if(field[row][col] === EMPTY && iteration !== -1) {
+        if(iteration%2 === 0) {
+            field[row][col] = ZERO;
+            iteration++;
+            renderSymbolInCell(ZERO, row, col);
+        }
+        else {
+             field[row][col] = CROSS;
+             iteration++;
+             renderSymbolInCell(CROSS,row, col);
+        }
+    }
+
+    let winner = Winner();
+    if (winner === CROSS || winner === ZERO){
+        WinnerAler(winner)
+    }
+
     console.log(`Clicked on cell: ${row}, ${col}`);
+}
 
+function WinnerAler(winner){
+    if(iteration !== -1) {
+        iteration = -1;
+        recolorField(winner); 
+        alert(`${winner === CROSS ? "Крестики" : "Нолики"} победили!`);
+    }
+}
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+function recolorField (symbol){
+    for(let i = 0; i<3 ; i++) {
+        for(let j = 0; j<3 ; j++)
+            if(field[i][j] === symbol)
+                renderSymbolInCell(symbol, i, j,'#FF0000');
+    }
+}
+
+function Winner(){
+    let crossField = field.reduce((f,row) => f + row.join('').toString().replace(/O/g,EMPTY),'');
+    let zeroField = field.reduce((f,row) => f + row.join('').toString().replace(/X/g,EMPTY),'').replace(/O/g,CROSS);
+    if(WIN_POSITIONS.includes(crossField))
+        return CROSS;
+    else if(WIN_POSITIONS.includes(zeroField))
+        return ZERO;
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -55,6 +120,7 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+    startGame();
 }
 
 
