cdf2d2812621d94b90c2453ab824923e4e568cc4
Coll4pse <Coll4pse@users.noreply.github.com>
2020-09-14 20:54:10 +0500

    Done

--- index.js
+++ index.js
@@ -1,14 +1,117 @@
+class GameField{
+    constructor(rows = 3, cols = 3){
+        console.log(`in constructor`);
+        this.field = [];
+        
+        console.log(`before cycle`);
+        for(let i = 0 ; i < cols; i++){
+            let tempRowsArray = [];
+            for(let j =0 ; j < rows; j++){
+                tempRowsArray.push(EMPTY);   
+            }
+            this.field.push(tempRowsArray);
+        }
+        console.log(`after cycle`);
+    }
+
+    makeNextMove(row, col){
+        let cross = this.markCount(CROSS);
+        let zero = this.markCount(ZERO);
+        console.log(`crosses: ${cross} Zeros: ${zero}`);
+        if(cross > zero){
+            this.field[row][col] = ZERO;
+            return ZERO;
+        }
+        this.field[row][col] = CROSS;
+        return CROSS;
+    }
+
+    markCount(mark){
+        let count =0;
+        for(let i = 0 ; i < this.field.length; i++)
+            for(let j = 0 ; j < this.field[0].length; j++)
+                if(this.field[i][j] === mark)
+                    count++;
+        return count;
+    }
+
+    isCellEmpty(row, col) {
+        return this.field[row][col] === EMPTY;
+    }
+
+    isGameEnd() {
+        return this.isSomeoneWin() || this.isDraw();
+    }
+
+    isDraw() {
+        for(let i = 0 ; i < this.field.length; i++)
+            for(let j = 0 ; j < this.field[0].length; j++)
+                if (this.field[i][j] === EMPTY)
+                    return false;
+        return true;
+    }
+    
+    fillWinnerInRed() {
+        let winner = this.isSomeoneWin();
+        if(!winner) return;
+        for(const cell of winner){
+            renderSymbolInCell(cell.mark, cell.i, cell.j, "red");
+        }
+    }
+
+    isSomeoneWin() {
+        let MainDiagArr = [];
+        let SecDiagArr = [];
+
+        for (let i = 0; i < this.field.length; i++) {
+            let rowArr = [];
+            let colArr = [];
+            
+            for (let j = 0; j < this.field[0].length; j++) {
+                rowArr.push({mark:this.field[i][j], i,j});
+                colArr.push({mark:this.field[j][i], i:j,j:i});
+            }
+            
+            let rowTempValue = rowArr[0];
+            let colTempValue = colArr[0];
+            
+            if (rowTempValue.mark !== EMPTY && rowArr.every(elem => elem.mark === rowTempValue.mark))
+                return rowArr;
+
+            if (colTempValue.mark !== EMPTY && colArr.every(elem => elem.mark === colTempValue.mark))
+                return colArr;
+            
+            MainDiagArr.push({mark: this.field[i][i], i:i, j:i});
+            SecDiagArr.push({mark: this.field[this.field.length - i - 1][i], i:this.field.length - i - 1, j:i});
+        }
+
+        if (MainDiagArr[0].mark !== EMPTY && MainDiagArr.every(elem => elem.mark === MainDiagArr[0].mark))
+            return MainDiagArr;
+        if (SecDiagArr[0].mark !== EMPTY && SecDiagArr.every(elem => elem.mark === SecDiagArr[0].mark))
+            return SecDiagArr;
+
+        return false;
+    }
+}
+
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
 
+let variousDimm = prompt("Введи размерность поля", 3)
+
+const globalDimm = !Number.isNaN(Number(variousDimm)) && Number(variousDimm) > 2? Number(variousDimm) : 3;
 const container = document.getElementById('fieldWrapper');
 
-startGame();
+let globalField;
+
+startGame(globalDimm);
 addResetListener();
 
-function startGame () {
-    renderGrid(3);
+function startGame (dimension) {
+    renderGrid(dimension);
+    globalField = new GameField(dimension, dimension);
+    console.log(`field: ${globalField.field}`);
 }
 
 function renderGrid (dimension) {
@@ -27,13 +130,23 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
+    console.log(`isDraw: ${globalField.isDraw()}, isWin: ${globalField.isSomeoneWin()}`);
+    if(!globalField.isCellEmpty(row, col) || globalField.isDraw() || globalField.isSomeoneWin())
+        return;
+    let moveMark = globalField.makeNextMove(row, col);
+    console.log(moveMark);
+    renderSymbolInCell(moveMark, row, col);
+
     console.log(`Clicked on cell: ${row}, ${col}`);
 
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    if(globalField.isSomeoneWin()){
+        alert(`Наш победитель ${globalField.isSomeoneWin()[0].mark}`);
+        globalField.fillWinnerInRed();
+        return;
+    }
+    if(globalField.isDraw()){
+        alert("Победила дружба!");
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -55,6 +168,7 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+    startGame (globalDimm);
 }
 
 
