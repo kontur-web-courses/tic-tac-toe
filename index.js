a63b99f58e31aef3a47c79435c9dff5c439c6d0f
Deemjan <orda1500@gmail.com>
2020-09-25 20:34:47 +0500

    Tasks completed

--- index.js
+++ index.js
@@ -4,36 +4,216 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
-startGame();
-addResetListener();
+const gameField = [[],[],[]];
+let clickCounter = 0;
+let dimension;
+let maxTurnCounter;
+let aiTurn = false;
+let gameEnded = false;
+let fieldExpand;
 
-function startGame () {
-    renderGrid(3);
+addResetListener();
+addStartListener();
+
+function initGameField(dimension, gameField){
+    for (let i = 0; i < dimension; i++){
+        gameField[i] = new Array(dimension);
+        for (let j = 0; j<dimension; j++){
+            gameField[i][j] = EMPTY;
+        }
+    }
+    console.log(gameField)
 }
 
-function renderGrid (dimension) {
-    container.innerHTML = '';
+function getDimensions(){
+    let input = document.getElementById('fieldSizeInput');
+    let dimensions = parseInt(input.value);
+    if (!isNaN(dimensions))
+        return dimensions;
+    return 3;
+}
 
+function startGame (dimension) {
+    initGameField(dimension, gameField)
+    renderGrid(dimension);
+    fieldExpand = document.getElementById('enableFieldExpanding').checked;
+}
+
+function renderGrid (dimension, redraw=false) {
+    container.innerHTML = '';
+    let enableDumbAI = document.getElementById('enableDumbAi').checked;
+    let enableSmartAI = document.getElementById('enableSmartAi').checked;
     for (let i = 0; i < dimension; i++) {
         const row = document.createElement('tr');
         for (let j = 0; j < dimension; j++) {
             const cell = document.createElement('td');
-            cell.textContent = EMPTY;
-            cell.addEventListener('click', () => cellClickHandler(i, j));
+            cell.textContent = redraw ? gameField[i][j] : EMPTY;
+            cell.addEventListener('click', () =>
+                (enableDumbAI || enableSmartAI) ?
+                    cellClickHandlerButWithAI(i, j, enableSmartAI) : cellClickHandler(i, j));
             row.appendChild(cell);
         }
         container.appendChild(row);
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
+function checkWinner(gameField){
+    checkHorizontalWinner(gameField);
+
+    checkVerticalWinner(gameField);
+
+    checkDiagonalWinner(gameField);
+}
+
+function paintWinningFields(line, startIndex, col = false) {
+    if (col){
+        for (let i = 0; i < line.length; i++) {
+            findCell(i, startIndex).style.color = 'red'
+        }
+        return
+    }
+
+    for (let i = 0; i < line.length; i++) {
+        findCell(startIndex, i).style.color = 'red'
+    }
+}
+
+function paintDiagonal(line,main = true){
+    if(main){
+        for (let i = 0; i < line.length; i++){
+            findCell(i,i).style.color = 'red';
+        }
+        return
+    }
+    for (let i = 0; i < line.length; i++){
+        findCell(line.length-1-i,i).style.color = 'red';
+    }
+}
+
+function checkVerticalWinner(gameField){
+    for(let i=0;i<gameField.length;i++){
+        let flatArray = gameField.flat(2)
+        let word = ''
+        for (let j = i; j < flatArray.length; j+=gameField.length) {
+            if(flatArray[j]===EMPTY)
+                continue
+            word += flatArray[j]
+        }
+        if (gameEnded)
+            break;
+
+        if(word===CROSS.repeat(gameField.length)){
+            alert(`${CROSS} победил`)
+            paintWinningFields(gameField, i, true)
+            break
+        }
+        else if(word===ZERO.repeat(gameField.length)){
+            alert(`${ZERO} победил`)
+            paintWinningFields(gameField, i, true)
+            break
+        }
+    }
+}
+
+function createDiagonalLines(gameField) {
+    let mainDiagonal = '';
+    let secondaryDiagonal = '';
+    for (let i = 0; i < gameField.length; i++) {
+        if (gameField[i][i] !== EMPTY)
+            mainDiagonal += gameField[i][i];
+        if (gameField[gameField.length - 1 - i][i] !== EMPTY)
+            secondaryDiagonal += gameField[gameField.length - 1 - i][i];
+    }
+    return {mainDiagonal, secondaryDiagonal};
+}
+
+function checkDiagonalWinner(gameField) {
+    if(gameEnded) return;
+
+    let {mainDiagonal, secondaryDiagonal} = createDiagonalLines(gameField);
+
+    let main = false
+    let crossLine = CROSS.repeat(gameField.length);
+    let zeroLine = ZERO.repeat(gameField.length);
+
+    if ((mainDiagonal===crossLine || mainDiagonal===zeroLine))
+        main = true
+
+    if ((mainDiagonal===crossLine || secondaryDiagonal===crossLine)){
+        alert(`${CROSS} победил`)
+        gameEnded= true
+        if(main)
+            paintDiagonal(gameField)
+        else
+            paintDiagonal(gameField,main)
+    }
+    else if((mainDiagonal===zeroLine || secondaryDiagonal===zeroLine)){
+        gameEnded= true
+        if(main)
+            paintDiagonal(gameField)
+        else
+            paintDiagonal(gameField,main)
+        alert(`${ZERO} победил`)
+    }
+}
+
+function checkHorizontalWinner(gameField) {
+    for (let i = 0; i< gameField.length;i++){
+        let row = gameField[i].join("")
+        if (row === CROSS.repeat(gameField.length) && !gameEnded ){
+            gameEnded = true
+            paintWinningFields(row, i)
+            alert(`Победил ${CROSS}`)
+            break
+        }
+        if (row === ZERO.repeat(gameField.length) && !gameEnded){
+            gameEnded = true
+            paintWinningFields(row, i)
+            alert(`Победил ${ZERO}`)
+            break
+        }
+    }
+}
+
+function makeNextTurn(symbol,row,col){
+    gameField[row][col] = symbol;
     console.log(`Clicked on cell: ${row}, ${col}`);
+    clickCounter++;
+    renderSymbolInCell(symbol, row, col);
 
+    checkWinner(gameField)
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    if (gameEnded)
+        return;
+
+    if(fieldExpand && clickCounter >= Math.round(maxTurnCounter/2))
+        increaseField();
+
+    if (clickCounter === maxTurnCounter && !gameEnded)
+        alert('Победила дружба')
+}
+
+function cellClickHandler(row, col){
+    if(gameField[row][col] !== EMPTY || gameEnded){
+        return
+    }
+
+    let symbol = clickCounter % 2 === 0 ? CROSS : ZERO;
+    makeNextTurn(symbol,row, col);
+}
+
+function cellClickHandlerButWithAI(row,col, enableSmartAI){
+    if(gameField[row][col] !== EMPTY || gameEnded){
+        return
+    }
+    let symbol = aiTurn ? ZERO : CROSS;
+    makeNextTurn(symbol,row,col)
+
+    if (aiTurn) {
+        aiTurn = false;
+        return
+    }
+    randomAiTurn(enableSmartAI);
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -48,13 +228,130 @@ function findCell (row, col) {
     return targetRow.querySelectorAll('td')[col];
 }
 
+function findEmptyCells(){
+    let emptyCells = []
+    for (let i = 0; i < dimension; i++){
+        for (let j = 0; j<dimension; j++){
+            if (gameField[i][j] === EMPTY)
+                emptyCells.push({x:i, y:j});
+        }
+    }
+
+    return emptyCells
+}
+
+function randomAiTurn(enableSmartAI){
+    if(gameEnded)
+        return;
+    aiTurn = true;
+    let emptyCells = findEmptyCells();
+    let randomInt = generateRandomInt(emptyCells)
+    let selectedCell;
+    if(enableSmartAI){
+        let smartCellPick = smartAiTurn(emptyCells);
+        selectedCell = typeof(smartCellPick) === 'undefined' ? emptyCells[randomInt] : smartCellPick;
+    }
+    else
+        selectedCell = emptyCells[randomInt];
+    clickOnCell(selectedCell.x, selectedCell.y)
+}
+
+function smartAiTurn(emptyCells){
+    let fakeField = gameField.map((gameField)=> gameField.slice());
+    for (let cell of emptyCells){
+        fakeField[cell.x][cell.y] = ZERO;
+        if(guessDiagonalWinner(fakeField)
+            || guessIfHorizontalWinner(fakeField)
+            || guessIfVerticalWinner(fakeField)){
+            return cell;
+        }
+        fakeField[cell.x][cell.y] = EMPTY;
+    }
+}
+
+function guessIfHorizontalWinner(gameField) {
+    for (let i = 0; i< gameField.length;i++){
+        let row = gameField[i].join("")
+        if(gameEnded)
+            break
+        if (row === CROSS.repeat(gameField.length) || row === ZERO.repeat(gameField.length))
+            return true;
+    }
+    return false;
+}
+
+function guessIfVerticalWinner(gameField){
+    for(let i=0;i<gameField.length;i++){
+        if(gameEnded)
+            return;
+
+        let flatArray = gameField.flat(2)
+        let word = ''
+
+        for (let j = i; j < flatArray.length; j+=gameField.length) {
+            if(flatArray[j]===EMPTY)
+                continue
+            word += flatArray[j]
+        }
+
+        if(word===CROSS.repeat(gameField.length) || word===ZERO.repeat(gameField.length)){
+            return true
+        }
+    }
+    return false;
+}
+
+function increaseField() {
+    gameField.unshift((new Array(dimension+1)).join(EMPTY).split(''));
+    gameField.push((new Array(dimension+1)).join(EMPTY).split(''));
+    for(let i = 0; i < gameField.length; i++){
+        gameField[i].unshift(EMPTY)
+        gameField[i].push(EMPTY)
+    }
+    dimension+=2;
+    maxTurnCounter = dimension*dimension;
+    renderGrid(dimension, true);
+}
+
+function guessDiagonalWinner(gameField) {
+    let {mainDiagonal, secondaryDiagonal} = createDiagonalLines(gameField);
+
+    let crossLine = CROSS.repeat(gameField.length);
+    let zeroLine = ZERO.repeat(gameField.length);
+
+    return (mainDiagonal===crossLine || secondaryDiagonal===crossLine)
+        || (mainDiagonal===zeroLine || secondaryDiagonal===zeroLine)
+}
+
+function generateRandomInt(emptyCells) {
+    return Math.floor(Math.random() * emptyCells.length);
+
+}
+
 function addResetListener () {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
+function addStartListener () {
+    const startButton = document.getElementById('start');
+    startButton.addEventListener('click', startClickHandler);
+}
+
 function resetClickHandler () {
     console.log('reset!');
+    gameEnded = false;
+    clickCounter = 0;
+    startGame(dimension)
+}
+
+function startClickHandler () {
+    console.log('gameStarted!');
+    gameEnded = false;
+    clickCounter = 0;
+    dimension = getDimensions();
+    maxTurnCounter = dimension*dimension;
+    startGame(dimension)
 }
 
 
