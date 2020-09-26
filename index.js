d89ac26df71f817953ba540f2bc4d36e799a1a8c
Tyset <em3.14ty5e7@gmail.com>
2020-09-26 17:54:50 +0500

    Up to 6 done

--- index.js
+++ index.js
@@ -4,11 +4,32 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+let field;
+let rows;
+let cols;
+let mainDiag;
+let antiDiag;
+let xTurn;
+let endState;
+
+let dim;
+
 startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    dim = Number(prompt("Enter dimension", 3));
+    field = [];
+    for (let i = 0; i < dim; i++)
+        field.push(new Array(dim).fill(EMPTY));
+    rows = new Array(dim).fill(0);
+    cols = new Array(dim).fill(0);
+    xTurn = true;
+    endState = false;
+    turnCount = 0;
+    mainDiag = 0;
+    antiDiag = 0;
+    renderGrid(dim);
 }
 
 function renderGrid (dimension) {
@@ -28,11 +49,43 @@ function renderGrid (dimension) {
 
 function cellClickHandler (row, col) {
     // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
+
+    if (!endState && field[row][col] == EMPTY) {
+
+        renderSymbolInCell(xTurn ? CROSS : ZERO, row, col);
+        turnCount += 1;
+
+        let modifier = xTurn ? 1 : -1;
+
+        rows[row] += modifier;
+        cols[col] += modifier;
+
+        if (row == col)
+            mainDiag += modifier;
+        
+        if (dim - 1 - row == col)
+            antiDiag += modifier;
+        
+        if (Math.abs(rows[row]) == dim || Math.abs(cols[col]) == dim || Math.abs(mainDiag) == dim || Math.abs(antiDiag) == dim) {
+            alert(`${xTurn ? "X" : "O"} won!`);
+            endState = true;
+            return;
+        } else if (turnCount == dim ** 2) {
+            alert("Friendship won!");
+            endState = true;
+            return;
+        }
+
+        xTurn = !xTurn;
+        field[row][col] = xTurn ? CROSS : ZERO;
+
+        console.log(`Clicked on cell: ${row}, ${col}, Values: ${rows}, ${cols}, Diags: ${mainDiag}, ${antiDiag}`);
+
+    }
 
 
     /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
+        
      */
 }
 
@@ -55,6 +108,7 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+    startGame()
 }
 
 
