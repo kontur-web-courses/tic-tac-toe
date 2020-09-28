49986dc836c0734ebf37c6aedd624a62edd77842
Tyset <em3.14ty5e7@gmail.com>
2020-09-29 01:37:28 +0400

    Done, zero refactoring :(

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ class Rule {
         this.antiDiagIdx = 2 * d + 1;
     }
 
+    
     setValue(row, col, value) {
         this.full[this.rowIdx + row] = value;
         this.full[this.colIdx + col] = value;
@@ -22,6 +23,7 @@ class Rule {
         if (this.d - 1 - row == col)
             this.full[this.antiDiagIdx] = value;
     }
+    
 
     turnChanges(row, col, modifier = 1) {
         this.full[this.rowIdx + row] += modifier;
@@ -31,7 +33,86 @@ class Rule {
         if (this.d - 1 - row == col)
             this.full[this.antiDiagIdx] += modifier;
     }
+    
 
+    findTurn(f) {
+        let bestValue = Math.max(...this.full);
+        let rowIdxs = [];
+        let colIdxs = [];
+        let usingMain = this.full[this.mainDiagIdx] == bestValue;
+        let usingAnti = this.full[this.antiDiagIdx] == bestValue;
+
+        let allPossibleTurns = [];
+
+        for (let i = 0; i < this.full.length; i++) {
+            if (i < this.colIdx && this.full[i] == bestValue)
+                rowIdxs.push(i);
+            else if (this.full[i] == bestValue)
+                colIdxs.push(i - this.colIdx);
+        }
+
+        for (let i of rowIdxs){
+            for (let j = 0; j < this.d; j++){
+                allPossibleTurns.push([i, j]);
+            }
+        }
+
+
+        for (let i of colIdxs){
+            for (let j = 0; j < this.d; j++){
+                allPossibleTurns.push([j, i]);
+            }
+        }
+
+
+
+        if (usingMain) {
+            for (let i = 0; i < this.d; i++) {
+                allPossibleTurns.push([i, i]);
+            }
+        }
+
+
+        if (usingAnti) {
+            for (let i = 0; i < this.d; i++) {
+            allPossibleTurns.push([this.d - 1 - i, i]);
+            }
+        }
+
+
+        let allEmptyPossibleTurns = [];
+
+
+
+        for (let e of allPossibleTurns){
+            let r = e[0];
+            let c = e[1];
+            if (f[r][c] == EMPTY){
+                allEmptyPossibleTurns.push(e);
+            }
+        }
+
+        let valuesCount = {};
+        for (let i = 0; i < allEmptyPossibleTurns.length; i++) {
+            if (allEmptyPossibleTurns[i] in valuesCount) {
+                valuesCount[allEmptyPossibleTurns[i]] += 1;
+            } else {
+                valuesCount[allEmptyPossibleTurns[i]] = 1;
+            }
+        }
+
+        let maxValue = Number.NEGATIVE_INFINITY;
+        let maxKey;
+
+        for (let i in valuesCount){
+            if (valuesCount[i] > maxValue) {
+                maxValue = valuesCount[i];
+                maxKey = i;
+            }
+        }
+        return [maxKey, bestValue];
+    }
+    
     isWinState(row, col) {
         if (Math.abs(this.full[this.rowIdx + row]) == this.d || 
         Math.abs(this.full[this.colIdx + col]) == this.d || 
@@ -42,17 +123,34 @@ class Rule {
         return false;
     }
 
+    paintCells(field) {
+        // don't use until sure winState == true
+        let i = 0;
+        let winCombo = 0
+        while (Math.abs(this.full[i]) != this.d) {
+            i+=1;
+            winCombo = i;
+        }
 
-    findTurn(ignoreValue = Number.NEGATIVE_INFINITY) {
-        rowIdxs = [];
-        colIdxs = [];
-        usingMain = this.full[this.mainDiagIdx] != ignoreValue;
-        usingAnti = this.full[this.antiDiagIdx] != ignoreValue;
-        for (let i = 0; i < 2 * this.d; i++) {
-            if (i < this.colIdx && this.full[i] != ignoreValue)
-                rowIdxs.push(i);
-            else if (this.full[i + this.colIdx] != ignoreValue)
-                colIdxs.push(i - this.colIdx);
+        console.log(winCombo);
+
+        let winCells = [];
+        for (let i = 0; i < this.d; i++) {
+            if (winCombo < this.colIdx) {
+                winCells.push([winCombo, i]);
+            } else if (winCombo < this.mainDiagIdx) {
+                winCells.push([i, winCombo - this.colIdx])
+            } else if (winCombo == this.mainDiagIdx) {
+                winCells.push([i, i]);
+            } else {
+                winCells.push([this.d - 1 - i, i]);
+            }
+        }
+
+        for (let e of winCells) {
+            let r = e[0];
+            let c = e[1];
+            renderSymbolInCell(field[r][c], r, c, 'red');
         }
     }
 }
@@ -64,6 +162,10 @@ let endState;
 let turnCount;
 let multiPlayerRules;
 let dim;
+let aiMatch = true; // <= not Random AI
+
+let attack;
+let defense;
 
 startGame();
 addResetListener();
@@ -79,6 +181,11 @@ function startGame () {
     turnCount = 0;
     multiPlayerRules = new Rule(dim);
     renderGrid(dim);
+
+    if (aiMatch) {
+        attack = new Rule(dim);
+        defense = new Rule(dim);
+    }
 }
 
 function renderGrid (dimension) {
@@ -97,30 +204,82 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
     if (!endState && field[row][col] == EMPTY) {
-
         renderSymbolInCell(xTurn ? CROSS : ZERO, row, col);
-
         field[row][col] = xTurn ? CROSS : ZERO;
-
         turnCount += 1;
-
         multiPlayerRules.turnChanges(row, col, xTurn ? 1 : -1);
 
         endState = (multiPlayerRules.isWinState(row, col) || (turnCount == dim ** 2));
 
         if (endState) {
-            if (multiPlayerRules.isWinState(row, col))
+            if (multiPlayerRules.isWinState(row, col)) {
                 alert(`${xTurn ? "X" : "O"} won!`);
                 //paint here
-            else
+                multiPlayerRules.paintCells(field);
+            } else
                 alert("DRAW!");
             return;
         }
 
         xTurn = !xTurn;
-        // console.log(endState, multiPlayerRules.full, row, col)
+        if (aiMatch) {
+            // get row and col
+            attack.setValue(row, col, Number.NEGATIVE_INFINITY);
+            defense.turnChanges(row, col, 1);
+
+            let foundTurn = attack.findTurn(field);
+            let fullAttack = foundTurn[0].split(',').map((x) => Number(x));
+            let attackCoeff = foundTurn[1];
+            let aiRowAttack = fullAttack[0];
+            let aiColAttack = fullAttack[1];
+
+            foundTurn = defense.findTurn(field)
+            let fullDefense = foundTurn[0].split(',').map((x) => Number(x));
+            let aiRowDefense = fullDefense[0];
+            let aiColDefense = fullDefense[1];
+            let defenseCoeff = foundTurn[1];
+
+            let aiRowTurn = aiRowDefense;
+            let aiColTurn = aiColDefense;
+
+            if (defenseCoeff > attackCoeff){
+                aiRowTurn = aiRowDefense;
+                aiColTurn = aiColDefense;
+            } else {
+                aiRowTurn = aiRowAttack;
+                aiColTurn = aiColAttack;
+            }
+
+            console.log(attackCoeff, defenseCoeff);
+
+            
+
+            attack.turnChanges(aiRowTurn, aiColTurn, 1);
+            //attack.setValue(aiRowTurn, aiColTurn, Number.NEGATIVE_INFINITY);
+
+            defense.setValue(aiRowTurn, aiColTurn, Number.NEGATIVE_INFINITY);
+            
+            renderSymbolInCell(xTurn ? CROSS : ZERO, aiRowTurn, aiColTurn);
+            field[aiRowTurn][aiColTurn] = xTurn ? CROSS : ZERO;
+            multiPlayerRules.turnChanges(aiRowTurn, aiColTurn, xTurn ? 1 : -1);
+
+            turnCount += 1;
+
+            endState = (multiPlayerRules.isWinState(aiRowTurn, aiColTurn) || (turnCount == dim ** 2));
+
+            if (endState) {
+                if (multiPlayerRules.isWinState(aiRowTurn, aiColTurn)) {
+                    alert(`${xTurn ? "X" : "O"} won!`);
+                    //paint here
+                    multiPlayerRules.paintCells(field);
+                }   else
+                    alert("DRAW!");
+                return;
+            }
+
+            xTurn = !xTurn;
+        }
     }
 }
 
