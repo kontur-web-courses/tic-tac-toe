114265474b8b1f8d544c56c40810a9e6c7809f4d
unknown <katya.shyb@yandex.ru>
2020-09-29 16:26:59 +0500

    Шубинцева 1-10

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
+let field;
+let player = CROSS;
+let freeCells;
+let dimension = 3;
+let win = false;
 
 const container = document.getElementById('fieldWrapper');
 
@@ -8,7 +13,17 @@ startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    dimension = parseInt(prompt("Введите размер стороны поля:", 3));
+    renderGrid(dimension);
+    freeCells = dimension ** 2;
+    field = new Array();
+    win = false; 
+    for(var i=0; i<dimension; i++){
+        field[i] = new Array();
+        for(var j=0; j<dimension; j++){
+            field[i][j] = EMPTY;
+        }
+    }
 }
 
 function renderGrid (dimension) {
@@ -27,13 +42,124 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
+    //console.log(`Clicked on cell: ${row}, ${col}`);
+    if (player === ZERO){
+        let ai = AI(field)
+        row = ai.x;
+        col = ai.y;
+    }  
 
+    if(field[row][col] !== EMPTY || win)
+        return;
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    renderSymbolInCell(player, row, col);
+    field[row][col] = player;
+    
+    if (--freeCells <= 0){
+        alert('Победила дружба!');
+    };
+    
+    let cells = [];
+    for (let i = 0; i<=dimension-1;i++){
+        cells.length = 0;
+        for (let j = 0; j <=dimension-1;j++){
+            if (field[i][j] === player){
+                let obj = {x:i, y:j};
+                cells.push(obj);
+                if (cells.length === dimension){
+                    alert('Победил ' + player);
+                    brush(cells); 
+                    win = true;                       
+                    return;
+                }
+            }
+            else {
+                cells.length = 0;
+            }
+        }
+    }
+
+    for (let i = 0; i<=dimension-1;i++){
+        cells.length = 0;
+        for (let j = 0; j <=dimension-1;j++){
+            if (field[j][i] === player){
+                let obj = {x:j, y:i};
+                cells.push(obj);
+                if (cells.length === dimension){
+                    alert('Победил ' + player);
+                    brush(cells); 
+                    win = true;                        
+                    return;
+                }
+            }
+            else {
+                cells.length = 0;
+            }
+        }
+    }
+
+    for (let i = 0; i<=dimension-1;i++){
+        for (let j = 0; j <=dimension-1;j++){
+            if (i === j){
+                if (field[i][j] === player){
+                    let obj = {x:i, y:j};
+                    cells.push(obj);
+                    if (cells.length === dimension){
+                        alert('Победил ' + player);
+                        brush(cells);  
+                        win = true;                       
+                        return;
+                    }
+                }
+                else {
+                    cells.length = 0;
+                }
+            }
+        }
+    }
+
+    for (let i = dimension-1; i>=0;i--){
+        for (let j = 0; j <=dimension-1;j++){
+            if (i+j === dimension-1){
+                if (field[i][j] === player){
+                    let obj = {x:i, y:j};
+                    cells.push(obj);
+                    if (cells.length === dimension){
+                        alert('Победил ' + player);
+                        brush(cells);   
+                        win = true;                      
+                        return;
+                    }
+                }
+                else {
+                    cells.length = 0;
+                }
+            }
+        }
+    }
+
+    if (player === CROSS){
+        player = ZERO;    
+    }
+    else{
+        player = CROSS;
+    }
+}
+
+function AI (field) {
+    let obj = {x:getRandomInt(3), y:getRandomInt(3)}
+    if (field[obj.x][obj.y] != EMPTY){
+        return AI(field);
+    }
+    else {
+        return obj;
+    } 
+}
+
+function brush (cells){
+    for(var i of cells){
+        renderSymbolInCell(player, i.x, i.y, 'red');
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -54,9 +180,13 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
+    startGame();
     console.log('reset!');
 }
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 
 /* Test Function */
 /* Победа первого игрока */
