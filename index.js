068f61a6344f563fa41524eff8791248dec84356
Sc222 <v.a.shaternikov@gmail.com>
2020-10-03 00:19:26 +0500

    tasks 1-3 and hover animation

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const ZERO = 'O';
 const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
+let currentSymbol = undefined;
+let field = undefined;
+let gridSize = 3;
+
 
 startGame();
 addResetListener();
 
-function startGame () {
-    renderGrid(3);
+//testWin();
+
+function startGame() {
+    gridSize = parseInt(prompt("Введите размер поля", "3")) || 3;
+    currentSymbol = CROSS;
+    renderGrid(gridSize);
+    //rows cols
+    field = Array(gridSize).fill().map(() => Array(gridSize).fill(EMPTY));
+    console.log(field);
 }
 
-function renderGrid (dimension) {
+function renderGrid(dimension) {
     container.innerHTML = '';
-
     for (let i = 0; i < dimension; i++) {
         const row = document.createElement('tr');
         for (let j = 0; j < dimension; j++) {
@@ -26,41 +36,40 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
+function cellClickHandler(row, col) {
     console.log(`Clicked on cell: ${row}, ${col}`);
-
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    if (field[row][col] === EMPTY) {
+        field[row][col] = currentSymbol;
+        renderSymbolInCell(currentSymbol, row, col);
+        currentSymbol = currentSymbol === CROSS ? ZERO : CROSS;
+    }
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
-
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
-    console.log('reset!');
+function resetClickHandler() {
+    startGame();
 }
 
 
 /* Test Function */
+
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -71,7 +80,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -84,6 +93,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
