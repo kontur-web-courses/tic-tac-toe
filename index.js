f80aff7bbdad78ae2beccab7f3bc7e3ca06d8665
filippusha <55542758+filippusha@users.noreply.github.com>
2020-10-06 00:27:44 +0500

    Филипп Максимов
    Ф

--- index.js
+++ index.js
@@ -4,11 +4,30 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+let gameField;
+let counter;
+let hasWinner;
+
 startGame();
 addResetListener();
 
-function startGame () {
-    renderGrid(3);
+function initGameField(dimension) {
+    gameField = [];
+    counter = 0;
+    hasWinner = false;
+    for (let i = 0; i < dimension; i++) {
+        gameField[i] = new Array(dimension);
+        for (let j = 0; j < dimension; j++) {
+            gameField[i][j] = EMPTY;
+        }
+    }
+    console.log(gameField, 'initialized field');
+}
+
+function startGame (dimension = 3) {
+    let dim = Number(prompt("Введите ширину поля"));
+    initGameField(dim);
+    renderGrid(dim);
 }
 
 function renderGrid (dimension) {
@@ -26,14 +45,149 @@ function renderGrid (dimension) {
     }
 }
 
+function makeStringFixed(symbol, length) {
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += symbol;
+    }
+    return result;
+}
+
+function isFieldFull () {
+    let dimension = gameField.length;
+    for (let i = 0; i < dimension; i++) {
+        for (let j = 0; j < dimension; j++) {
+            if (gameField[i][j] === EMPTY) return false;
+        }
+    }
+    return true;
+}
+
+function paintCell (row, col, color) {
+    const targetCell = findCell(row, col);
+    targetCell.style.color = color;
+}
+
+function isWinnerVertical (symbol) {
+    let checkString = '';
+    let dimension = gameField.length;
+    let winnerStreak = symbol === CROSS ? makeStringFixed(CROSS, dimension) : makeStringFixed(ZERO, dimension);
+    for (let j = 0; j < dimension; j++) {
+        checkString = '';
+        for (let i = 0; i < dimension; i++) {
+            checkString += gameField[i][j];
+        }
+        if (checkString === winnerStreak) {
+            for (let i = 0; i < dimension; i++) {
+                paintCell(i, j, '#ff0000');
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+function isWinnerHorizontal (symbol) {
+    let checkString = '';
+    let dimension = gameField.length;
+    let winnerStreak = symbol === CROSS ? makeStringFixed(CROSS, dimension) : makeStringFixed(ZERO, dimension);
+    for (let i = 0; i < dimension; i++) {
+        checkString = '';
+        for (let j = 0; j < dimension; j++) {
+            checkString += gameField[i][j];
+        }
+        if (checkString === winnerStreak) {
+            for (let j = 0; j < dimension; j++) {
+                paintCell(i, j, '#ff0000');
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+function isWinnerDiagonal (symbol) {
+    let checkStringMain = '';
+    let checkStringSide = '';
+    let dimension = gameField.length;
+    let winnerStreak = symbol === CROSS ? makeStringFixed(CROSS, dimension) : makeStringFixed(ZERO, dimension);
+    for (let i = 0; i < dimension; i++) {
+        for (let j = 0; j < dimension; j++) {
+            if (i === j) {
+                checkStringMain += gameField[i][j];
+                checkStringSide += gameField[i][dimension - j - 1];
+            }
+        }
+    }
+    if (checkStringMain === winnerStreak) {
+        for (let i = 0; i < dimension; i++) {
+            paintCell(i, i, '#ff0000');
+        }
+        return true;
+    }
+    else if (checkStringSide === winnerStreak) {
+        for (let i = 0; i < dimension; i++) {
+            paintCell(i, dimension - i - 1, '#ff0000');
+        }
+        return true;
+    }
+    else return false;
+}
+
+function isWinner (symbol) {
+    return isWinnerDiagonal(symbol) || isWinnerVertical(symbol) || isWinnerHorizontal(symbol);
+}
+
+function getRandomInRange(max) {
+    return Math.round(Math.random() * 10000000000) % max;
+}
+
+function computerMove () {
+    let row = 0;
+    let col = 0;
+    let dimension = gameField.length;
+    let flag = true;
+    while (flag) {
+        row = getRandomInRange(dimension);
+        col = getRandomInRange(dimension);
+        if (gameField[row][col] === EMPTY) {
+            flag = false
+        }
+    }
+    gameField[row][col] = ZERO;
+    renderSymbolInCell(ZERO, row, col);
+    console.log(`Computer put ZERO on cell: ${row}, ${col}`);
+    if (isWinner(ZERO)) {
+        alert(`Победили ${ZERO}`);
+        hasWinner = true;
+    }
+    else if (isFieldFull()) {
+        alert('Победила дружба');
+    }
+}
+
 function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
-
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    if (hasWinner) return;
+    if (gameField[row][col] !== EMPTY) {
+        console.log(`Cell [${row},${col}] is already marked`)
+        return;
+    }
+    let symbol = CROSS;
+    //counter++;
+    gameField[row][col] = symbol;
+    renderSymbolInCell(symbol, row, col);
+    //console.log(`Clicked on cell: ${row}, ${col}, ${symbol}`);
+    console.log(`Player put CROSS on cell: ${row}, ${col}`);
+    if (isWinner(symbol)) {
+        alert(`Победили ${symbol}`);
+        hasWinner = true;
+    }
+    else if (isFieldFull()) {
+        alert('Победила дружба');
+    }
+    else {
+        setTimeout(computerMove, 500);
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -54,9 +208,9 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
+    startGame();
     console.log('reset!');
 }
-
 
 /* Test Function */
 /* Победа первого игрока */
