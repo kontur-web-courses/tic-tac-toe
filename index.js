526224c055ef47413064743c2efd228a5adaa64d
costya0805 <51258799+costya0805@users.noreply.github.com>
2020-10-26 01:16:12 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
-
+let gameMap = [];
+let emptyCells = 0;
+let gameIsEnd = false;
+let winCells = [];
+let mapSize = 0;
+let player = 0;
 const container = document.getElementById('fieldWrapper');
+let autoExpansion = true;
+let useAi = true;
 
 startGame();
 addResetListener();
 
 function startGame () {
-    renderGrid(3);
+    mapSize = Number(document.getElementById("fieldSizeValue").value);
+    autoExpansion = document.getElementById("expansionChecked").checked;
+    useAi = document.getElementById("useAiChecked").checked;
+    gameMap = [];
+    renderGrid(mapSize);
+    for (let i = 0; i< mapSize; i++){
+        gameMap.push(new Array(mapSize).fill(EMPTY));
+    }
+    emptyCells = mapSize**2;
 }
 
 function renderGrid (dimension) {
@@ -27,13 +42,109 @@ function renderGrid (dimension) {
 }
 
 function cellClickHandler (row, col) {
-    // Пиши код тут
+    if (gameMap[row][col] !== EMPTY || gameIsEnd)
+        return;
+    gameMap[row][col] = player%2 === 0? CROSS: ZERO;
+    renderSymbolInCell(gameMap[row][col],row,col);
+    checkWinner(row, col);
+    if (gameIsEnd){
+        winCells.forEach(element => {
+            let row = element[0]
+            let col = element[1]
+            renderSymbolInCell(gameMap[row][col],row,col, 'red');
+        });
+        alert(`Победил ${player%2 === 0? CROSS: ZERO}`);
+        return;
+    }
+    emptyCells--;
+    if (autoExpansion && emptyCells < (mapSize**2) / 2){
+        increaseMap();
+    }
     console.log(`Clicked on cell: ${row}, ${col}`);
+    if(emptyCells === 0){
+        alert("Победила дружба");
+        return;
+    }
+    player++;
+    if (useAi && player%2 === 1){
+        AiMove()
+    }
+}
 
+function AiMove(){
+    possibleCells = []
+    for (let row = 0; row < mapSize; row++)
+        for (let col = 0; col < mapSize; col++)
+            if (gameMap[row][col] === EMPTY)
+                possibleCells.push([row,col]);
+    let rand = Math.floor(Math.random() * possibleCells.length);
+    cellClickHandler(possibleCells[rand][0], possibleCells[rand][1])
+}
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+function increaseMap(){
+    mapSize++;
+    emptyCells += mapSize**2-(mapSize-1)**2; 
+    renderGrid(mapSize);
+    let newGameMap = [];
+    for (let i = 0; i< mapSize; i++){
+        newGameMap.push(new Array(mapSize).fill(EMPTY));
+    }
+    let row = 0;
+    let col = 0;
+    gameMap.forEach(element => {
+        element.forEach(sym => {
+            renderSymbolInCell(sym,row,col);
+            newGameMap[row][col] = sym;
+            col++;
+        });
+        row++;
+        col = 0;
+    });
+    gameMap = newGameMap;
+}
+
+function checkWinner(row,col){
+    for (let i = row - 1; i <= row + 1; i++){
+        if (i - 1 >= 0 && i + 1 < mapSize){
+            gameIsEnd = gameMap[i - 1][col] === gameMap[i][col] && gameMap[i][col] === gameMap[i+1][col];
+            if (gameIsEnd){
+                winCells = [[i - 1, col], [i, col], [i + 1, col]];
+                return;
+            }
+        }
+    }
+
+    for (let i = col - 1; i <= col + 1; i++){
+        if (i - 1 >= 0 && i + 1 < mapSize){
+            gameIsEnd = gameMap[row][i-1] === gameMap[row][i] 
+            && gameMap[row][i] === gameMap[row][i+1];
+            if (gameIsEnd){
+                winCells = [[row, i - 1], [row, i], [row, i+1]];
+                return;
+            }
+        }
+    }
+
+    for (let i = -1; i <= 1; i++){
+        if (row + i - 1 >= 0 && col + i - 1 >=0  && row + i + 1 < mapSize && col + i + 1 < mapSize){
+            gameIsEnd = gameMap[row + i - 1][col + i - 1] === gameMap[row + i][col + i] && gameMap[row + i][col + i] === gameMap[row + i + 1][col + i + 1];
+            if (gameIsEnd){
+                winCells = [[row + i - 1, col + i - 1], [row + i, col + i], [row + i + 1, col + i + 1]];
+                return;
+            }
+        }
+    }
+
+    for (let i = -1; i <= 1; i++){
+        if (row + i - 1 >= 0 && col - (i - 1) >=0  && row + i + 1 < mapSize && col - (i + 1) < mapSize){
+            gameIsEnd = gameMap[row + i - 1][col - (i - 1)] === gameMap[row + i][col - i] && gameMap[row + i][col - i] === gameMap[row + i + 1][col - (i + 1)];
+            if (gameIsEnd){
+                winCells = [[row + i - 1, col - (i - 1)], [row + i, col - i], [row + i + 1, col - (i + 1)]];
+                return;
+            }
+        }
+    }
+    
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -54,6 +165,10 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
+    winCells = []
+    gameIsEnd = false;
+    player = 0;
+    startGame()
     console.log('reset!');
 }
 
