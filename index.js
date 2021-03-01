20223bbca1f8b04307dedfde468025844e3bfaa1
MexBanDoc <tvb060201@gmail.com>
2021-03-01 15:30:02 +0500

    Все основное и кривой бот!

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+let field = [[EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY]];
+let turn = 0;
+let turnCounter = 0;
+let isEnd = false;
+let II = false;
+
+
 startGame();
 addResetListener();
 
 function startGame() {
+    let size = prompt('Введите размер поля', '3');
+    if (parseInt(size) !== 3) {
+        alert('Вы молодец! Но играть будете на поле 3х3!');
+    }
+    II = confirm('Играть с ИИ?');
     renderGrid(3);
 }
-
-let a = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]];
-let turn = 0;
-let turnCounter = 0;
-let isEnd = false;
 
 
 function renderGrid(dimension) {
@@ -36,22 +45,26 @@ function cellClickHandler(row, col) {
     // Пиши код тут
     console.log(`Clicked on cell: ${row}, ${col}`);
     if (!isEnd) {
-        if (a[row][col] === EMPTY) {
+        if (field[row][col] === EMPTY) {
             if (turn === 0) {
                 renderSymbolInCell(ZERO, row, col);
-                a[row][col] = ZERO;
+                field[row][col] = ZERO;
             } else {
                 renderSymbolInCell(CROSS, row, col);
-                a[row][col] = CROSS;
+                field[row][col] = CROSS;
             }
             turn = (turn + 1) % 2;
             setTimeout(checkWinner, 100);
         }
     }
+    if (II) {
+        makeTurnII();
+    }
     /* Пользоваться методом для размещения символа в клетке так:
         renderSymbolInCell(ZERO, row, col);
      */
 }
+
 
 function checkDruzba() {
     turnCounter++;
@@ -64,29 +77,29 @@ function checkDruzba() {
 
 function checkWinner() {
     let winner = 'None';
-    if (a[0][0] === a[0][1] && a[0][1] === a[0][2] && a[0][0] !== EMPTY) {
-        winner = a[0][1] === 'X' ? 'CROSS' : 'ZERO';
+    if (field[0][0] === field[0][1] && field[0][1] === field[0][2] && field[0][0] !== EMPTY) {
+        winner = field[0][1] === 'X' ? 'CROSS' : 'ZERO';
         recolorLineToRed(0, 0, 0, 1, 0, 2);
-    } else if (a[1][0] === a[1][1] && a[1][1] === a[1][2] && a[1][0] !== EMPTY) {
-        winner = a[1][1] === 'X' ? 'CROSS' : 'ZERO';
+    } else if (field[1][0] === field[1][1] && field[1][1] === field[1][2] && field[1][0] !== EMPTY) {
+        winner = field[1][1] === 'X' ? 'CROSS' : 'ZERO';
         recolorLineToRed(1, 0, 1, 1, 1, 2);
-    } else if (a[2][0] === a[2][1] && a[2][1] === a[2][2] && a[2][0] !== EMPTY) {
-        winner = a[2][1] === 'X' ? 'CROSS' : 'ZERO';
+    } else if (field[2][0] === field[2][1] && field[2][1] === field[2][2] && field[2][0] !== EMPTY) {
+        winner = field[2][1] === 'X' ? 'CROSS' : 'ZERO';
         recolorLineToRed(2, 0, 2, 1, 2, 2);
-    } else if (a[0][0] === a[1][0] && a[1][0] === a[2][0] && a[2][0] !== EMPTY) {
-        winner = a[0][0] === 'X' ? 'CROSS' : 'ZERO';
+    } else if (field[0][0] === field[1][0] && field[1][0] === field[2][0] && field[2][0] !== EMPTY) {
+        winner = field[0][0] === 'X' ? 'CROSS' : 'ZERO';
         recolorLineToRed(0, 0, 1, 0, 2, 0);
-    } else if (a[0][1] === a[1][1] && a[1][1] === a[2][1] && a[2][1] !== EMPTY) {
-        winner = a[0][1] === 'X' ? 'CROSS' : 'ZERO';
+    } else if (field[0][1] === field[1][1] && field[1][1] === field[2][1] && field[2][1] !== EMPTY) {
+        winner = field[0][1] === 'X' ? 'CROSS' : 'ZERO';
         recolorLineToRed(0, 1, 1, 1, 2, 1);
-    } else if (a[0][2] === a[1][2] && a[1][2] === a[2][2] && a[2][2] !== EMPTY) {
-        winner = a[0][2] === 'X' ? 'CROSS' : 'ZERO';
+    } else if (field[0][2] === field[1][2] && field[1][2] === field[2][2] && field[2][2] !== EMPTY) {
+        winner = field[0][2] === 'X' ? 'CROSS' : 'ZERO';
         recolorLineToRed(0, 2, 1, 2, 2, 2);
-    } else if (a[0][0] === a[1][1] && a[1][1] === a[2][2] && a[2][2] !== EMPTY) {
-        winner = a[1][1] === 'X' ? 'CROSS' : 'ZERO';
+    } else if (field[0][0] === field[1][1] && field[1][1] === field[2][2] && field[2][2] !== EMPTY) {
+        winner = field[1][1] === 'X' ? 'CROSS' : 'ZERO';
         recolorLineToRed(0, 0, 1, 1, 2, 2);
-    } else if (a[0][2] === a[1][1] && a[1][1] === a[2][0] && a[2][0] !== EMPTY) {
-        winner = a[1][1] === 'X' ? 'CROSS' : 'ZERO';
+    } else if (field[0][2] === field[1][1] && field[1][1] === field[2][0] && field[2][0] !== EMPTY) {
+        winner = field[1][1] === 'X' ? 'CROSS' : 'ZERO';
         recolorLineToRed(0, 2, 1, 1, 2, 0);
     }
     if (!checkDruzba() && winner !== 'None') {
@@ -127,46 +140,69 @@ function addResetListener() {
 
 function resetClickHandler() {
     console.log('reset!');
-    a = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]];
+    field = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]];
     turn = 0;
     turnCounter = 0;
     isEnd = false;
-    for (let i = 0; i < a.length; i++) {
-        for (let j = 0; j < a.length; j++) {
+    II = false;
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field.length; j++) {
             renderSymbolInCell(EMPTY, i, j);
         }
     }
 
 }
 
+function makeTurnII() {
+    let row = Math.floor(Math.random() * 3);
+    let col = Math.floor(Math.random() * 3);
+    let flag = false;
+    while (!flag) {
+        if (!isEnd) {
+            if (field[row][col] === EMPTY) {
+                if (turn === 0) {
+                    renderSymbolInCell(ZERO, row, col);
+                    field[row][col] = ZERO;
+                } else {
+                    renderSymbolInCell(CROSS, row, col);
+                    field[row][col] = CROSS;
+                }
+                turn = (turn + 1) % 2;
+                setTimeout(checkWinner, 100);
+                flag = true;
+            }
+        }
+    }
 
-/* Test Function */
 
-/* Победа первого игрока */
-function testWin() {
-    clickOnCell(0, 2);
-    clickOnCell(0, 0);
-    clickOnCell(2, 0);
-    clickOnCell(1, 1);
-    clickOnCell(2, 2);
-    clickOnCell(1, 2);
-    clickOnCell(2, 1);
-}
+    /* Test Function */
 
-/* Ничья */
-function testDraw() {
-    clickOnCell(2, 0);
-    clickOnCell(1, 0);
-    clickOnCell(1, 1);
-    clickOnCell(0, 0);
-    clickOnCell(1, 2);
-    clickOnCell(1, 2);
-    clickOnCell(0, 2);
-    clickOnCell(0, 1);
-    clickOnCell(2, 1);
-    clickOnCell(2, 2);
-}
+    /* Победа первого игрока */
+    function testWin() {
+        clickOnCell(0, 2);
+        clickOnCell(0, 0);
+        clickOnCell(2, 0);
+        clickOnCell(1, 1);
+        clickOnCell(2, 2);
+        clickOnCell(1, 2);
+        clickOnCell(2, 1);
+    }
 
-function clickOnCell(row, col) {
-    findCell(row, col).click();
+    /* Ничья */
+    function testDraw() {
+        clickOnCell(2, 0);
+        clickOnCell(1, 0);
+        clickOnCell(1, 1);
+        clickOnCell(0, 0);
+        clickOnCell(1, 2);
+        clickOnCell(1, 2);
+        clickOnCell(0, 2);
+        clickOnCell(0, 1);
+        clickOnCell(2, 1);
+        clickOnCell(2, 2);
+    }
+
+    function clickOnCell(row, col) {
+        findCell(row, col).click();
+    }
 }
