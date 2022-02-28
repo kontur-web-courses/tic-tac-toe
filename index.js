41cee184c82a891ffdeae34b376ac30f4162b5f7
Nastja0 <anastasizta_mgn3003@inbox.ru>
2022-02-28 19:24:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
+let number = 0;
+let run = true;
 
 const container = document.getElementById('fieldWrapper');
 
@@ -9,6 +11,8 @@ addResetListener();
 
 function startGame () {
     renderGrid(3);
+    number = 0;
+    run = true;
 }
 
 function renderGrid (dimension) {
@@ -25,17 +29,49 @@ function renderGrid (dimension) {
         container.appendChild(row);
     }
 }
-let number = 0
+
 function cellClickHandler (row, col) {
-    if (number % 2 == 0) renderSymbolInCell(CROSS, row, col);
-    else renderSymbolInCell(ZERO, row, col);
-    number += 1;
-    console.log(`Clicked on cell: ${row}, ${col}`);
+    if (run && findCell(row, col).textContent == EMPTY) 
+    {
+        if (number % 2 == 0) renderSymbolInCell(CROSS, row, col);
+        else renderSymbolInCell(ZERO, row, col);
+        number += 1;
+        let res = getWinner();
+        if (res != EMPTY) alert(res);
+        else if (number == 9) alert('Победила дружба')
+    }
+        
+}
 
+function getWinner() {
+    let sim = []
+    for(let i = 0; i < 3; i++)
+    {
+        sim.push(check_line([i, 0], [i, 1], [i, 2]));
+        sim.push(check_line([0, i], [1, i], [2, i]));
+    }
+    sim.push(check_line([0,0],[1,1],[2,2]));
+    sim.push(check_line([0,2],[1,1],[2.0]));
+    console.log(sim);
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    if (sim.includes(CROSS)) return CROSS;
+    if (sim.includes(ZERO)) return ZERO;
+    return EMPTY;
+}
+
+function check_line(x1, x2, x3)
+{
+    if (findCell(x1[0], x1[1]).textContent == findCell(x2[0], x2[1]).textContent && findCell(x2[0], x2[1]).textContent == findCell(x3[0], x3[1]).textContent && findCell(x2[0], x2[1]).textContent != EMPTY)
+    {
+        let sim = findCell(x1[0], x1[1]).textContent;
+
+        renderSymbolInCell(sim, x1[0], x1[1], '#ff0000');
+        renderSymbolInCell(sim, x2[0], x2[1],  '#ff0000');
+        renderSymbolInCell(sim, x3[0], x3[1], '#ff0000');
+        run = false;
+        return sim;
+    }
+    return EMPTY;
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -56,6 +92,7 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
+    startGame();
     console.log('reset!');
 }
 
