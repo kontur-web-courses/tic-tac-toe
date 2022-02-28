fb0c275662120fdedac334184dcd62ea11c1440b
igor <ighor.naumov.2002mail@mail.ru>
2022-02-28 13:15:35 +0500

    update

--- index.js
+++ index.js
@@ -1,17 +1,87 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
-
 const container = document.getElementById('fieldWrapper');
+let board;
+
+class Board {
+    constructor(size) {
+        this.size = size;
+        this.board = new Array(size);
+        this.symbol = CROSS;
+        this.steps = 0;
+    }
+
+    pasteSymbol(row, col) {
+        this.board[row * this.size + col] = this.symbol;
+        let previousSymbol = this.symbol;
+        this.symbol = this.symbol === CROSS ? ZERO : CROSS;
+        this.steps += 1;
+        return previousSymbol;
+    }
+
+    isEmpty(row, col) {
+        return !this.board[row * this.size + col];
+    }
+
+    isTie() {
+        return this.steps === (this.size * this.size);
+    }
+
+    isWinner() {
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size - 1; j++) {
+                if (this.board[i + j * this.size] !== this.board[i + j * this.size + this.size]) {
+                    break;
+                }
+
+                if (j === (this.size - 2))
+                    return this.board[i + j * this.size + this.size];
+            }
+        }
+
+        for (let i = 0; i < this.size * this.size; i += this.size) {
+            for (let j = 0; j < this.size - 1; j++) {
+                if (this.board[i + j] !== this.board[i + j + 1]) {
+                    break;
+                }
+
+                if (j === (this.size - 2))
+                    return this.board[i + j + 1];
+            }
+        }
+
+        for (let i = 0; i < this.size; i += this.size + 1){
+            if (this.board[i] !== this.board[i + this.size + 1]){
+                break;
+            }
+
+            if (i === this.size + 1){
+                return this.board[i];
+            }
+        }
+
+        for (let i = 2; i < this.size; i += this.size -= 1){
+            if (this.board[i] !== this.board[i + this.size -1]){
+                break;
+            }
+
+            if (i === this.size - 1){
+                return this.board[i];
+            }
+        }
+    }
+}
 
 startGame();
 addResetListener();
 
-function startGame () {
+function startGame() {
     renderGrid(3);
+    board = new Board(3);
 }
 
-function renderGrid (dimension) {
+function renderGrid(dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -26,41 +96,49 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
+function cellClickHandler(row, col) {
     console.log(`Clicked on cell: ${row}, ${col}`);
+    if (board.isEmpty(row, col)) {
+        let symbol = board.pasteSymbol(row, col)
+        renderSymbolInCell(symbol, row, col);
+    }
 
+    if (board.isTie()) {
+        alert(`Победила дружба`);
+    }
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    let winner = board.isWinner();
+    if (winner) {
+        alert(`Победил ${winner}`)
+    }
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+function resetClickHandler() {
     console.log('reset!');
 }
 
 
 /* Test Function */
+
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -71,7 +149,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -84,6 +162,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
