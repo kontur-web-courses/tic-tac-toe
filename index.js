d1d66da2e5333dcc6083445c47d2baf547514d84
Vladik74 <vlad06012002@mail.ru>
2022-02-28 19:33:42 +0500

    blablabla

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
+let count = 0;
+let field = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]];
+let gameover = false;
+let width = 3;
 
 const container = document.getElementById('fieldWrapper');
 
 startGame();
 addResetListener();
 
-function startGame () {
+function startGame() {
     renderGrid(3);
 }
 
-function renderGrid (dimension) {
+function renderGrid(dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -26,41 +30,142 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
+function cellClickHandler(row, col) {
+    if (!gameover) {
+        if (field[row][col] === EMPTY) {
+            if (count % 2 === 0) {
+                renderSymbolInCell(ZERO, row, col);
+                field[row][col] = ZERO;
+            } else {
+                renderSymbolInCell(CROSS, row, col);
+                field[row][col] = CROSS;
+            }
+            count += 1;
+            console.log(`Clicked on cell: ${row}, ${col}`);
+        }
+        let symbol = ZERO;
+        if (count % 2 === 0)
+            symbol = CROSS;
+        if (checkWinner(symbol)) {
+            gameover = true;
+            alert(`Player ${count % 2} win!`)
+        }
+        if (count === 9) {
+            alert("Победила дружба!");
+        }
+    }
 
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function checkWinner(symbol) {
+    let flag = true;
+    let cur = field[0][0];
+    let row = -1;
+    for (let i = 0; i < width; i++) {
+        cur = field[i][0];
+        flag = true;
+        row = i;
+        for (let j = 1; j < width; j++) {
+            if (field[i][j] !== cur) {
+                flag = false;
+            }
+        }
+
+        if (flag && cur !== EMPTY) {
+            for (let k = 0; k < width; k++)
+                renderSymbolInCell(symbol, row, k, '#ff1100');
+            return true;
+
+        }
+    }
+
+    for (let i = 0; i < width; i++) {
+        cur = field[0][i];
+        flag = true;
+        for (let j = 1; j < width; j++) {
+            if (field[j][i] !== cur) {
+                flag = false;
+            }
+        }
+        if (flag && cur !== EMPTY) {
+            for (let k = 0; k < width; k++)
+                renderSymbolInCell(symbol, k, i, '#ff1100');
+            return true;
+        }
+    }
+
+    cur = field[0][0];
+    flag = true;
+    for (let i = 0; i < width; i++) {
+        if (field[i][i] !== cur) {
+            flag = false;
+        }
+    }
+    if (flag && cur !== EMPTY) {
+        for (let i = 0; i < width; i++) {
+            renderSymbolInCell(symbol, i, i, '#ff1100')
+        }
+        return true;
+    }
+
+    cur = field[width - 1][0];
+    flag = true;
+    for (let i = 0; i < width; i++) {
+        if (field[width - i - 1][i] !== cur) {
+            flag = false;
+        }
+    }
+    if (flag && cur !== EMPTY) {
+        for (let i = 0; i < width; i++) {
+            renderSymbolInCell(symbol, width - i - 1, i, '#ff1100');
+        }
+        return true;
+    }
+
+    return false;
+
+
+}
+
+function colorCells(symbol) {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (field[i][j] === symbol)
+                renderSymbolInCell(symbol, i, j, '#ff1100')
+        }
+    }
+}
+
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+function resetClickHandler() {
+    count = 0;
+    field = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]];
+    gameover = false;
+    renderGrid(3);
     console.log('reset!');
 }
 
 
 /* Test Function */
+
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -71,7 +176,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -84,6 +189,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
