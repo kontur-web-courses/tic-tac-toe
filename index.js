a90187d80a9da6098dde31f6318fec9523b1e740
Evgeny Khoroshavin <themplarer@gmail.com>
2022-02-28 19:49:03 +0500

    trash

--- index.js
+++ index.js
@@ -3,12 +3,47 @@ const ZERO = 'O';
 const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
+let FIELD_SIZE = 3;
+let emptyCount = 0;
+let isCrossesTurn = true;
+let isGameEnded = false;
+const RED = '#EF342D';
+let emptyCells = new Set();
+
+let points = new Map(
+	[[CROSS, -1],
+	 [EMPTY, 0],
+	 [ZERO, 1]]);
+
+let players = new Map()
+	.set(-1, CROSS)
+	.set(1, ZERO)
+	.set(0, EMPTY);
 
 startGame();
 addResetListener();
+let field = initField(FIELD_SIZE);
+
+function initField(size){	
+	let field = [];
+	for (let i = 0; i < size; i++){
+		let row = [];
+		for (let j = 0; j < size; j++) {
+			// emptyCells.add((i, j));
+			console.log(emptyCells.size);
+			row.push(EMPTY);
+			renderSymbolInCell(EMPTY, i, j);
+		}
+
+		field.push(row);
+	}
+	
+	emptyCount = size ** 2;
+	return field;
+}
 
 function startGame () {
-    renderGrid(3);
+    renderGrid(FIELD_SIZE);
 }
 
 function renderGrid (dimension) {
@@ -26,14 +61,46 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
+function cellClickHandler (row, col) {	
+	if (field[row][col] !== EMPTY || isGameEnded) return;
+	
     console.log(`Clicked on cell: ${row}, ${col}`);
+	field[row][col] = isCrossesTurn ? CROSS : ZERO;
+	isCrossesTurn = !isCrossesTurn;
+	renderSymbolInCell(field[row][col], row, col);
+	emptyCells.delete((row, col));
+	console.log(emptyCells.size);
+	
+	let winner = checkWinner();
+	if (winner !== 0) {
+		let winnersSymbol = players.get(winner);
+		alert(winnersSymbol);
+		isGameEnded = true;
+	}
+	
+	if (--emptyCount === 0) {
+		alert('Победила дружба!');
+		isGameEnded = true;
+	}
+	
+	// let turn = getSmartAITurn();
+	// console.log(`Clicked on cell: ${turn[0]}, ${turn[1]}`);
+	// field[turn[0]][turn[1]] = isCrossesTurn ? CROSS : ZERO;
+	// isCrossesTurn = !isCrossesTurn;
+	// renderSymbolInCell(field[turn[0]][turn[1]], turn[0], turn[1]);
+	// emptyCells.delete(turn);
 
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+	// winner = checkWinner();
+	// if (winner !== 0) {
+		// let winnersSymbol = players.get(winner);
+		// alert(winnersSymbol);
+		// isGameEnded = true;
+	// }
+	
+	// if (emptyCells.size === 0) {
+		// alert('Победила дружба!');
+		// isGameEnded = true;
+	// }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -51,10 +118,87 @@ function findCell (row, col) {
 function addResetListener () {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
+	
+	const setSizeButton = document.getElementById('setSize');
+    setSizeButton.addEventListener('click', setSize);
 }
 
 function resetClickHandler () {
     console.log('reset!');
+	
+	field = initField(FIELD_SIZE);
+	isGameEnded = false;
+	isCrossesTurn = true;
+}
+
+function setSize () {
+    const number = parseInt(document.getElementById('number').value);
+
+	FIELD_SIZE = number;
+	startGame(FIELD_SIZE);
+	field = initField(FIELD_SIZE);
+}
+
+function checkWinner(){
+	let sum = 0;
+	
+	for (let i = 0; i < FIELD_SIZE; i++){
+		sum = 0;
+		for (let j = 0; j < FIELD_SIZE; j++)
+			sum += points.get(field[i][j]);
+		
+		if (Math.abs(sum) === FIELD_SIZE){
+			for (let j = 0; j < FIELD_SIZE; j++){
+				renderSymbolInCell(field[i][j], i, j, RED);
+			}
+			return Math.sign(sum);
+		}
+	}
+	
+	for (let i = 0; i < FIELD_SIZE; i++){
+		sum = 0;
+		for (let j = 0; j < FIELD_SIZE; j++)
+			sum += points.get(field[j][i]);
+		
+		if (Math.abs(sum) === FIELD_SIZE){
+			for (let j = 0; j < FIELD_SIZE; j++){
+				renderSymbolInCell(field[j][i], j, i, RED);
+			}
+			return Math.sign(sum);
+		}
+	}
+	
+	sum = 0;
+
+	for (let i = 0; i < FIELD_SIZE; i++){
+		sum += points.get(field[i][i]);
+	}
+	
+	if (Math.abs(sum) === FIELD_SIZE){
+		for (let j = 0; j < FIELD_SIZE; j++){
+			renderSymbolInCell(field[j][j], j, j, RED);
+		}
+		return Math.sign(sum);
+	}
+	
+	sum = 0;
+	for (let i = 0; i < FIELD_SIZE; i++){
+		sum += points.get(field[i][FIELD_SIZE - i - 1]);
+	}
+	
+	if (Math.abs(sum) === FIELD_SIZE){
+		for (let j = 0; j < FIELD_SIZE; j++){
+			renderSymbolInCell(field[j][FIELD_SIZE - j - 1], j, FIELD_SIZE - j - 1, RED);
+		}
+		
+		return Math.sign(sum);
+	}
+	
+	return 0;
+}
+
+function getSmartAITurn(){
+	return [...emptyCells][Math.floor(Math.random() * emptyCells.size)];
 }
 
 
