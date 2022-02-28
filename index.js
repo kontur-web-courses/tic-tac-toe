5503815df2abb68050661d0e9e4e432a98b451de
aburgart02 <73283354+aburgart02@users.noreply.github.com>
2022-02-28 12:42:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
-
+const winningConditions =
+    [[[0, 0], [0, 1], [0, 2]],
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
+    [[0, 0], [1, 0], [2, 0]],
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]],
+    [[0, 0], [1, 1], [2, 2]],
+    [[0, 2], [1, 1], [2, 0]]
+    ]
+let turn = 0;
+let wincolor = []
+let isWin = false;
+let arr = [[0, 0, 0],
+           [0, 0, 0],
+           [0, 0, 0]];
 const container = document.getElementById('fieldWrapper');
 
 startGame();
@@ -13,7 +28,6 @@ function startGame () {
 
 function renderGrid (dimension) {
     container.innerHTML = '';
-
     for (let i = 0; i < dimension; i++) {
         const row = document.createElement('tr');
         for (let j = 0; j < dimension; j++) {
@@ -26,13 +40,75 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
+function getFirstWinner()
+{
+    for (let winPosition of winningConditions) {
+        let count = 0
+        for (let pos of winPosition) {
+            if (arr[pos[0]][pos[1]] === 1) {
+                count += 1
+            }
+        }
+        if (count === 3){
+            wincolor = winPosition
+            return true
+        }
+    }
+}
 
+function getSecondWinner()
+{
+    for (let winPosition of winningConditions) {
+        let count = 0
+        for (let pos of winPosition) {
+            if (arr[pos[0]][pos[1]] === 2) {
+                count += 1
+            }
+        }
+        if (count === 3){
+            wincolor = winPosition
+            return true
+        }
+    }
+}
+
+function cellClickHandler (row, col) {
+    if (isWin)
+        return
+    if (arr[row][col] === 0) {
+        console.log(`Clicked on cell: ${row}, ${col}`);
+        if (turn % 2 === 0) {
+            renderSymbolInCell(CROSS, row, col);
+            arr[row][col] = 1;
+        }
+        else {
+            renderSymbolInCell(ZERO, row, col);
+            arr[row][col] = 2;
+        }
+        turn += 1;
+    }
+    let first = getFirstWinner()
+    let second = getSecondWinner()
+    if (first === true){
+        alert('Победил CROSS')
+        isWin = true
+        for (let x of wincolor)
+        {
+            renderSymbolInCell(CROSS, x[0], x[1], '#F00')
+        }
+    }
+    if (second === true){
+        alert('Победил ZERO')
+        isWin = true
+        for (let x of wincolor)
+        {
+            renderSymbolInCell(ZERO, x[0], x[1], '#F00')
+        }
+    }
+    if (turn === 9)
+        alert('Победила дружба')
 
     /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
      */
 }
 
@@ -55,6 +131,13 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+    renderGrid(3);
+    turn = 0;
+    wincolor = []
+    isWin = false;
+    arr = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]];
 }
 
 
