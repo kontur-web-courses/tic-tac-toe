706253a77608d8284aac92daf22d1fe7127744b0
AndreyKuklinov <Andreydolphin@mail.ru>
2023-02-27 18:15:58 +0500

    Запрет на изменение поставленного

--- index.js
+++ index.js
@@ -33,6 +33,9 @@ function renderGrid (dimension) {
 function cellClickHandler (row, col) {
     // Пиши код тут
     console.log(`Clicked on cell: ${row}, ${col}`);
+    if(board[row][col] !== EMPTY)
+      return;
+    board[row][col] = currentPlayer;
     renderSymbolInCell(currentPlayer, row, col);
     currentPlayer = getNextPlayer(currentPlayer);
 
