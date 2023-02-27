58c580deb93f931434ed4630535bcb7ba08585db
belutkautka <92267872+belutkautka@users.noreply.github.com>
2023-02-27 15:04:04 +0500

    add checkWinner

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const container = document.getElementById('fieldWrapper');
 startGame();
 addResetListener();
 
-function startGame () {
+function startGame() {
     field = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]]
     renderGrid(3);
 }
 
-function renderGrid (dimension) {
+function renderGrid(dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -28,7 +28,7 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
+function cellClickHandler(row, col) {
     // Пиши код тут
     console.log(`Clicked on cell: ${row}, ${col}`);
     const t = turn % 2 === 0 ? ZERO : CROSS;
@@ -45,32 +45,51 @@ function cellClickHandler (row, col) {
      */
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function checkWinner() {
+    for (let i = 0; i < 3; i++) {
+        if (field[i][0] === field[i][1] === field[i][2]) {
+            return [true,[i,0],[i,1],[i,2]];
+        }
+        if (field[0][i] === field[1][i] === field[2][i]) {
+            return [true,[0,i],[0,i],[0,i]];
+        }
+    }
+    if (field[0][0] === field[1][1] === field[2][2]) {
+        return [true,[0,0],[1,1],[2,2]];
+    }
+    if (field[0][2] === field[1][1] === field[2][0]) {
+        return [true,[0,2],[1,1],[2,0]];
+    }
+    return [false,0,0,0];
+}
+
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+function resetClickHandler() {
 
     console.log('reset!');
 }
 
 
 /* Test Function */
+
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -81,7 +100,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -94,6 +113,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
