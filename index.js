80e7e148fc13e885295e32026f7455e49f6bf099
Sokolova Anna <anna.sok.03@mail.ru>
2023-02-27 19:14:49 +0500

    done? not done!

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ const container = document.getElementById('fieldWrapper');
 
 startGame();
 addResetListener();
+testDraw();
+
+let board = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]];
+let turn = CROSS;
+let GameFinish = false;
 
 function startGame () {
+
+    board = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]];
     renderGrid(3);
+    turn = CROSS;
+    GameFinish = false;
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            renderSymbolInCell(board[i][j], i, j, '#333');
+        }
+    }
 }
 
 function renderGrid (dimension) {
@@ -26,10 +41,108 @@ function renderGrid (dimension) {
     }
 }
 
+function canMove() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] === EMPTY) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function paintHorizontal(row){
+    for (let i = 0; i < board[row].length; i++){
+        renderSymbolInCell(board[row][i], row, i, 'red');
+    }
+}
+
+function paintVertical(col){
+    for (let i = 0; i < 3; i++){
+        renderSymbolInCell(board[i][col], i, col, 'red');
+    }
+}
+
+function paintMainDiagonal() {
+    renderSymbolInCell(board[0][0], 0, 0, 'red');
+    renderSymbolInCell(board[1][1], 1, 1, 'red');
+    renderSymbolInCell(board[2][2], 2, 2, 'red');
+}
+
+function paintUnmainDiagonal() {
+    renderSymbolInCell(board[0][2], 0, 0, 'red');
+    renderSymbolInCell(board[1][1], 1, 1, 'red');
+    renderSymbolInCell(board[2][0], 2, 2, 'red');
+}
+
+function checkWinner(row, col) {
+    if (board[row][0] === board[row][1] && board[row][1] === board[row][2]) {
+        return 'row';
+    }
+
+    if (board[0][col] === board[1][col] && board[1][col] === board[2][col]) {
+        return 'col';
+    }
+
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== EMPTY) {
+        return 'mainDiagonal';
+    }
+
+    if (board[0][2] === board[1][1] && board[2][0] === board[1][1] && board[2][0] !== EMPTY) {
+        return 'unmainDiagonal';
+    }
+
+    return false;
+}
+
+function move (row, col) {
+    if (board[row][col] !== EMPTY) {
+        return false;
+    }
+
+    board[row][col] = turn;
+    renderSymbolInCell(turn, row, col);
+    return true;
+}
+
 function cellClickHandler (row, col) {
     // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
+    if (GameFinish) {
+        return;
+    }
 
+    let move = move();
+    if (move) {
+        let winnerMove = checkWinner(row, col);
+
+        if (winnerMove !== false) {
+            alert(turn);
+            switch (winnerMove) {
+                case 'col':
+                    paintVertical(col);
+                    break;
+                case 'row':
+                    paintVertical(row);
+                    break;
+                case 'mainDiagonal':
+                    paintMainDiagonal();
+                    break;
+                case 'unmainDiagonal':
+                    paintUnmainDiagonal();
+                    break;
+            }
+        }
+
+        if (!canMove() && winnerMove === false) {
+            alert('Победила дружба');
+            return;
+        }
+    }
+
+    turn = turn === CROSS ? ZERO : CROSS;
+    console.log(`Clicked on cell: ${row}, ${col}`);
 
     /* Пользоваться методом для размещения символа в клетке так:
         renderSymbolInCell(ZERO, row, col);
@@ -55,6 +168,8 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+    startGame();
+
 }
 
 
