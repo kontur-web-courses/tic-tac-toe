3e9f3f583695ebfca30bd9e96475003769773ed8
SolinMaxim <Maksim.Solin@urfu.me>
2023-02-27 19:15:27 +0500

    Base game

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
-
+let winnerExist = false;
 const container = document.getElementById('fieldWrapper');
+let field = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY],[EMPTY, EMPTY, EMPTY]];
+let currentMove = CROSS;
 
 startGame();
 addResetListener();
@@ -28,19 +30,85 @@ function renderGrid (dimension) {
 
 function cellClickHandler (row, col) {
     // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
+    if (field[row][col] === EMPTY && !winnerExist) {
+        renderSymbolInCell(currentMove, row, col);
+        winnerExist = checkWinner(row, col);
+        currentMove = currentMove === CROSS ? ZERO : CROSS;
 
-
+        console.log(`Clicked on cell: ${row}, ${col}`);
+        if (!winnerExist) {
+            let flag = false;
+            for (let row of field) {
+                for (let col of row) {
+                    if (col === EMPTY) {
+                        flag = true;
+                    }
+                }
+            }
+            if (!flag) {
+                alert('Победила дружба')
+            }
+        } else {
+            alert(field[row][col] + ' wins');
+        }
+    }
     /* Пользоваться методом для размещения символа в клетке так:
         renderSymbolInCell(ZERO, row, col);
      */
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
-    const targetCell = findCell(row, col);
 
+
+function checkWinner(row, col) {
+
+    if (field[row][0] === field[row][1] && field[row][1] === field[row][2]){
+        for (let i = 0; i < 3; i++)
+            renderSymbolInCell(field[row][i], row, i, '#FF0000');
+        return true;
+    }
+
+    if (field[0][col] === field[1][col] && field[1][col] === field[2][col]){
+        for (let i = 0; i < 3; i++)
+            renderSymbolInCell(field[i][col], i, col, '#FF0000');
+        return true;
+    }
+    if ((row + col) % 2 === 0){
+        if ( row === 1 && col === 1 ){
+            return checkMainDiagonal() || checkSecondDiagonal();
+        } else {
+            if (row === col) {
+                return checkMainDiagonal();
+            } else {
+                return checkSecondDiagonal();
+            }
+        }
+    }
+    return false;
+}
+
+function checkMainDiagonal(){
+    if (field[0][0] === field[1][1] && field[1][1] === field[2][2]) {
+        for (let i = 0; i < 3; i++) {
+            renderSymbolInCell(field[i][i], i, i, '#FF0000');
+        }
+    }
+
+    return (field[0][0] === field[1][1] && field[1][1] === field[2][2]);
+}
+
+function checkSecondDiagonal() {
+    if (field[2][0] === field[1][1] && field[1][1] === field[0][2]) {
+        for (let i = 0; i < 3; i++) {
+            renderSymbolInCell(field[i][2-i], i, 2-i, '#FF0000');
+        }
+    }
+    return  (field[2][0] === field[1][1] && field[1][1] === field[0][2]);
+}
+function renderSymbolInCell (symbol, row, col, color = '#333') {
+    const targetCell = findCell(row, col)
     targetCell.textContent = symbol;
     targetCell.style.color = color;
+    field[row][col] = symbol;
 }
 
 function findCell (row, col) {
@@ -54,6 +122,10 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
+    startGame();
+    field = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY],[EMPTY, EMPTY, EMPTY]];
+    winnerExist = false;
+    currentMove = CROSS;
     console.log('reset!');
 }
 
