6a8da85735911d09344858ad9d362307d31d072c
xyoouy <egorloparev@gmail.com>
2023-02-27 15:48:42 +0500

    Add 1-6

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const ZERO = 'O';
 const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
+const desk = [[] , [], []];
+let turnNumber = 0;
+let size = 0;
 
 startGame();
 addResetListener();
@@ -12,6 +15,7 @@ function startGame () {
 }
 
 function renderGrid (dimension) {
+    size = dimension
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -26,15 +30,114 @@ function renderGrid (dimension) {
     }
 }
 
+
+
 function cellClickHandler (row, col) {
-    // Пиши код тут
+
+    if (desk[row][col] !== undefined) {
+        return
+    }
+
+    if (turnNumber % 2 === 0) {
+        renderSymbolInCell(ZERO, row, col);
+        desk[row][col] = ZERO;
+        turnNumber += 1;
+    }
+    else {
+        renderSymbolInCell(CROSS, row, col);
+        desk[row][col] = CROSS;
+        turnNumber += 1;
+    }
+
     console.log(`Clicked on cell: ${row}, ${col}`);
 
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    if (checkWin(row, col))
+        alert(`${turnNumber % 2 + 1}`)
+    if (turnNumber === size * size)
+        alert('Победила дружба')
 }
+
+function checkWin(row, col)
+{
+    let winHorizontal = checkWinHorizontal(row, col)
+    let winVertical = checkWinVertical(row, col)
+    let winDiagonalFirst = checkWinDiagonalFirst(row, col)
+    let winDiagonalSecond = checkWinDiagonalSecond(row, col)
+
+    if (winHorizontal)
+    {
+        for(let i = 0; i < size; i++)
+        {
+            renderSymbolInCell(desk[row][i], row, i, '#ff0000')
+        }
+    }
+
+    if (winVertical)
+    {
+        for(let i = 0; i < size; i++)
+        {
+            renderSymbolInCell(desk[i][col], i, col, '#ff0000')
+        }
+    }
+
+    if (winDiagonalFirst)
+    {
+        for (let i = 0; i < size; i++) {
+            renderSymbolInCell(desk[i][i], i, i, '#ff0000')
+        }
+    }
+    if (winDiagonalSecond) {
+        let maxSize = size;
+        for (let i = 0; i < size; i++) {
+            renderSymbolInCell(desk[maxSize][i], maxSize, i, '#ff0000')
+            maxSize -= 1;
+        }
+    }
+
+    return winHorizontal || winVertical || winDiagonalFirst || winDiagonalSecond
+}
+
+function checkWinHorizontal(row, col)
+{
+    for(let i = 0; i < size; i++)
+    {
+        if (desk[row][i] !== desk[row][col])
+            return false;
+    }
+    return true;
+}
+
+function checkWinVertical(row, col)
+{
+    for(let i = 0; i < size; i++)
+    {
+        if (desk[i][col] !== desk[row][col])
+            return false;
+    }
+    return true;
+}
+
+function checkWinDiagonalFirst(row, col) {
+    for (let i = 0; i < size; i++) {
+        if (desk[i][i] !== desk[row][col])
+            return false;
+    }
+    return true;
+}
+
+function checkWinDiagonalSecond(row, col)
+{
+    let maxSize = size-1;
+    for(let i = 0; i < size; i++)
+    {
+        if (desk[maxSize][i] !== desk[row][col])
+            return false;
+        maxSize -= 1;
+    }
+    return true;
+}
+
+
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
