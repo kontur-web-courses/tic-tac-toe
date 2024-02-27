8121cdec1c47fadfd475d6302142ce30a2f24ba0
lordwartz <Lwartz@yandex.ru>
2024-02-27 20:13:00 +0500

    Implement game

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const CROSS = 'X';
 const ZERO = 'O';
 const EMPTY = ' ';
+const xo = [ZERO, CROSS];
+let curr_step = 1;
+let map = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]];
+let isEnd = false;
 
 const container = document.getElementById('fieldWrapper');
 
@@ -8,7 +12,10 @@ startGame();
 addResetListener();
 
 function startGame () {
+    isEnd = false;
     renderGrid(3);
+    curr_step = 1;
+    map = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]];
 }
 
 function renderGrid (dimension) {
@@ -28,12 +35,57 @@ function renderGrid (dimension) {
 
 function cellClickHandler (row, col) {
     // Пиши код тут
+    if (map[row][col] !== EMPTY || isEnd)
+        return;
     console.log(`Clicked on cell: ${row}, ${col}`);
 
+    if (curr_step >= 3 * 3) {
+        alert(`Победила дружба`);
+        isEnd = true;
+        return;
+    }
+
+    const curr_symbol = xo[curr_step % 2];
+    renderSymbolInCell(curr_symbol, row, col);
+    map[row][col] = curr_symbol;
+    curr_step++;
+    checkWinner(curr_symbol);
 
     /* Пользоваться методом для размещения символа в клетке так:
         renderSymbolInCell(ZERO, row, col);
      */
+}
+
+function checkDiagonalWinner(diagonal) {
+    let diag = [];
+    for (let i = 0; i < map.length; i++) {
+        diag[i] = diagonal ? map[i][i] : map[map.length - i - 1][i];
+    }
+    return diag.every(element => element === diag[0] && element !== EMPTY);
+}
+
+function checkWinner(cell){
+    const isWinRow = map.some(row => row.every(element => element === row[0] && element !== EMPTY));
+    let isWinCol = false;
+    for (let j = 0; j < map.length; j++) {
+        let col = [];
+        for (let i = 0; i < map.length; i++) {
+            col[i] = [map[i][j], [i, j]];
+        }
+        isWinCol = col.every(element => element === col[0] && element !== EMPTY);
+        if (isWinCol) {
+            break;
+        }
+
+    }
+
+    const isWinDiag1 = checkDiagonalWinner(false);
+    const isWinDiag2 = checkDiagonalWinner(true);
+
+    if (isWinRow || isWinCol || isWinDiag1 || isWinDiag2){
+        alert(`Победил ${cell}!`)
+        isEnd = true;
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -54,7 +106,8 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
-    console.log('reset!');
+    startGame();
+    console.log(`reset!, ${curr_step}`);
 }
 
 
