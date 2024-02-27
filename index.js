d49901e37f0d962a1ee30612c07d3c470fd0dec4
Andrey <andrey6458@yandex.ru>
2024-02-27 19:42:40 +0500

    complete 7

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const EMPTY = ' ';
 let count = 0;
 let pole = [];
 let k = 0;
+let stop = false;
 
 const container = document.getElementById('fieldWrapper');
 
@@ -33,17 +34,28 @@ function renderGrid (dimension) {
 function cellClickHandler (row, col) {
     console.log(`Clicked on cell: ${row}, ${col}`);
     k += 1;
-    if (findCell(row, col).textContent === EMPTY){
-        if (count === 0){
-            pole[row][col] = ZERO;
-            renderSymbolInCell(ZERO, row, col);
-            count = 1;
+    if (!stop) {
+        if (findCell(row, col).textContent === EMPTY){
+            if (count === 0){
+                pole[row][col] = ZERO;
+                renderSymbolInCell(ZERO, row, col);
+                count = 1;
+            }
+            else{
+                pole[row][col] = CROSS;
+                renderSymbolInCell(CROSS, row, col);
+                count = 0;
+            }
         }
-        else{
-            pole[row][col] = CROSS;
-            renderSymbolInCell(CROSS, row, col);
-            count = 0;
+    }
+
+    let b = checkWinner();
+    if (b[0]) {
+        stop = true;
+        for (let i = 0; i < b[1].length; i++) {
+            renderSymbolInCell(b[0], b[1][i][1], b[1][i][0], '#FF0000');
         }
+        alert('Победил ' + b[0])
     }
 
     if (k === pole[0].length * pole[0].length)
@@ -69,6 +81,84 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+}
+
+function checkWinner () {
+    let win = [];
+    let l = 0;
+    let a = '';
+    for (let i = 0; i < pole[0].length; i++) {
+        a = pole[i][0];
+        for (let j = 0; j < pole[0].length; j++) {
+            if (a === EMPTY) {
+                break;
+            }
+            if (pole[i][j] === a) {
+                win.push([i, j])
+                l += 1;
+            }
+        }
+        if (l === pole[0].length) {
+            return [pole[i][0], win];
+        }
+        l = 0;
+        win = [];
+    }
+
+    win = [];
+    a = '';
+    l = 0;
+    for (let i = 0; i < pole[0].length; i++) {
+        a = pole[0][i];
+        for (let j = 0; j < pole[0].length; j++) {
+            if (a === EMPTY) {
+                break;
+            }
+            if (pole[j][i] === a) {
+                win.push([i, j])
+                l += 1;
+            }
+        }
+        if (l === pole[0].length) {
+            return [pole[0][i], win];
+        }
+        l = 0;
+        win = [];
+    }
+
+    win = [];
+    l = 0;
+    a = pole[0][0];
+    for (let i = 0; i < pole[0].length; i++) {
+        if (a === EMPTY) {
+            break;
+        }
+        if (pole[i][i] === a) {
+            win.push([i, i])
+            l += 1;
+        }
+        if (l === pole[0].length) {
+            return [pole[0][0], win];
+        }
+    }
+
+    win = [];
+    l = 0;
+    a = pole[0][pole[0].length - 1];
+    for (let i = 0; i < pole[0].length; i++) {
+        if (a === EMPTY) {
+            break;
+        }
+        if (pole[i][pole[0].length - 1 - i] === a) {
+            win.push([i, pole[0].length - 1 - i])
+            l += 1;
+        }
+        if (l === pole[0].length) {
+            return [pole[0][pole[0].length - 1], win];
+        }
+    }
+
+    return [0, 0];
 }
 
 
