3cfb05a85cddee105e4a60de226f8016cf69d9c2
VladimirLezhnin <115662980+VladimirLezhnin@users.noreply.github.com>
2024-02-27 19:11:34 +0500

    prefinal

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const ZERO = 'O';
 const EMPTY = ' ';
 let Player = 1;
 const dimension = 3;
+let counter = 0;
 
 const container = document.getElementById('fieldWrapper');
 let gameBoard = [];
@@ -43,38 +44,62 @@ function renderGrid() {
 }
 
 function cellClickHandler(row, col) {
-    if (gameBoard[row][col] === EMPTY) {
+    if (gameBoard[row][col] === EMPTY && !isWinnerOnBoard().winner) {
         const symbol = Player === 1 ? ZERO : CROSS;
         renderSymbolInCell(symbol, row, col);
         gameBoard[row][col] = symbol;
-        if (isWinnerOnBoard())
+        let winnerCheck = isWinnerOnBoard();
+        if (winnerCheck.winner) {
             alert(`Победил игрок ${Player}`);
+            winnerCheck.line.forEach(([r, c]) => renderSymbolInCell(gameBoard[r][c], r, c, 'red'));
+        }
         Player = Player === 1 ? 2 : 1;
+        counter++;
+        if (counter === dimension ** 2 && !winnerCheck.winner)
+            alert('Победила дружба!!!')
     }
 }
+
 
 function isWinnerOnBoard() {
-    // Check rows and columns
+    // Check rows
     for (let i = 0; i < dimension; i++) {
-        if (
-            gameBoard[i].every(cell => cell === gameBoard[i][0] && cell !== EMPTY)  ||
-            gameBoard.every(row => row[i] === gameBoard[0][i] && row[i] !== EMPTY)
-        ) {
-            return true;
+        if (gameBoard[i].every(cell => cell === gameBoard[i][0] && cell !== EMPTY)) {
+            let line1 = [];
+            for (let j = 0; j < dimension; j++) {
+                line1.push([i, j])
+            }
+            return { winner: true, line: line1 };
         }
     }
-
-    // Check diagonals
-    if (
-        gameBoard.every((row, index) => row[index] === gameBoard[0][0] && row[index] !== EMPTY) ||
-        gameBoard.every((row, index) => row[dimension - index - 1] === gameBoard[0][dimension - 1]
-            && row[dimension - index - 1] !== EMPTY)
-    ) {
-        return true;
+    // Check columns
+    for (let i = 0; i < dimension; i++) {
+        if (gameBoard.every(row => row[i] === gameBoard[0][i] && row[i] !== EMPTY)) {
+            let line1 = [];
+            for (let j = 0; j < dimension; j++) {
+                line1.push([j, i])
+            }
+            return { winner: true, line: line1};
+        }
     }
-
-    return false;
+    // Check diagonals
+    if (gameBoard.every((row, index) => row[index] === gameBoard[0][0] && row[index] !== EMPTY)) {
+        let line1 = [];
+        for (let j = 0; j < dimension; j++) {
+            line1.push([j, j])
+        }
+        return { winner: true, line: line1 };
+    }
+    if (gameBoard.every((row, index) => row[dimension - index - 1] === gameBoard[0][dimension - 1] && row[dimension - index - 1] !== EMPTY)) {
+        let line1 = [];
+        for (let j = 0; j < dimension; j++) {
+            line1.push([j, dimension - 1 - j])
+        }
+        return { winner: true, line: line1 };
+    }
+    return { winner: false };
 }
+
 
 
 
@@ -97,8 +122,9 @@ function addResetListener() {
 
 function resetClickHandler() {
     Player = 1;
-    gameBoard = initializeBoard(3);
-    renderGrid(3);
+    gameBoard = initializeBoard(dimension);
+    renderGrid(dimension);
+    counter = 0;
 }
 
 /* Test Function */
