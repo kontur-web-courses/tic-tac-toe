cb546c53fc7cc7e5b42dc7cde4b1cbb016f6147b
Egor Grekhov <115657446+MidnightOwwl@users.noreply.github.com>
2024-02-27 19:17:42 +0500

    checkWinner

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+const field = new Array(3).fill(-1).map(() => new Array(3).fill(-1))
+
 startGame();
 addResetListener();
 
 function startGame () {
     renderGrid(3);
+}
+
+function getMove () {
+    let validMoves = []
+    for (let i = 0; i<3; i++){
+        for(let j = 0; j<3; j++){
+            if (field[i][j] === EMPTY){
+                validMoves.push([i, j])
+            }
+        }
+    }
+
+    const randomIndex = Math.floor(Math.random() * validMoves.length);
+    return validMoves[randomIndex]
 }
 
 function renderGrid (dimension) {
@@ -19,6 +35,7 @@ function renderGrid (dimension) {
         for (let j = 0; j < dimension; j++) {
             const cell = document.createElement('td');
             cell.textContent = EMPTY;
+
             cell.addEventListener('click', () => cellClickHandler(i, j));
             row.appendChild(cell);
         }
@@ -35,6 +52,37 @@ function cellClickHandler (row, col) {
         renderSymbolInCell(ZERO, row, col);
      */
 }
+
+function checkWinner() {
+    for (let i = 0; i < 3; i++) {
+        if (field[i][0] && field[i][0] === field[i][1] && field[i][0] === field[i][2]) {
+            return field[i][0];
+        }
+    }
+
+    for (let j = 0; j < 3; j++) {
+        if (field[0][j] && field[0][j] === field[1][j] && field[0][j] === field[2][j]) {
+            return field[0][j];
+        }
+    }
+
+    if (field[0][0] && field[0][0] === field[1][1] && field[0][0] === field[2][2]) {
+        return field[0][0];
+    }
+    if (field[0][2] && field[0][2] === field[1][1] && field[0][2] === field[2][0]) {
+        return field[0][2];
+    }
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (!field[i][j]) {
+                return '';
+            }
+        }
+    }
+    return 'Ничья';
+}
+
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
