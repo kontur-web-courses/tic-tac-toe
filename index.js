abd92ba5f6090289591b566612498ec8823fcdce
Danil <ferrum268@mail.ru>
2024-02-27 19:51:29 +0500

    finished!!!!!!!!!

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const EMPTY = ' ';
 const container = document.getElementById('fieldWrapper');
 
 let currentSymbol = CROSS;
-let N = 5;
 let gameEnded = false;
 let field;
+let fieldSize = 3;
+let aiMoved = false;
 
 startGame();
 addResetListener();
 
-function startGame () {
-    renderGrid(N);
+function startGame() {
     resetClickHandler();
 }
 
-function renderGrid (dimension) {
+function renderGrid(dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -43,86 +43,129 @@ function checkNoMoreMoves() {
     return true;
 }
 
-function findWinLine(symbol){
-    let winCells;
-    for (let i = 0; i < N; i++){
-        for (let j = 0; j < N - 2; j++){
-            if (field[i][j] === symbol && field[i][j+1] === symbol && field[i][j+2] === symbol){
-                winCells = [[i,j],[i,j+1],[i,j+2]];
+function findWinLine(symbol) {
+    let winCells = [];
+    for (let i = 0; i < fieldSize; i++) {
+        for (let j = 0; j < fieldSize - 2; j++) {
+            if (field[i][j] === symbol && field[i][j + 1] === symbol && field[i][j + 2] === symbol) {
+                winCells = [
+                    [i, j],
+                    [i, j + 1],
+                    [i, j + 2]
+                ];
             }
-            if (field[j][i] === symbol && field[j+1][i] === symbol && field[j+2][i] === symbol){
-                winCells = [[j, i],[j+1,i],[j+2,i]];
+            if (field[j][i] === symbol && field[j + 1][i] === symbol && field[j + 2][i] === symbol) {
+                winCells = [
+                    [j, i],
+                    [j + 1, i],
+                    [j + 2, i]
+                ];
             }
         }
     }
-    for (let i = 0; i < N - 2; i++){
-        for (let j = 0; j < N - 2; j++){
-            if (field[i][j] === symbol && field[i+1][j+1] === symbol && field[i+2][j+2] === symbol){
-                winCells = [[i,j],[i+1,j+1],[i+2,j+2]];
+
+    for (let i = 0; i < fieldSize - 2; i++) {
+        for (let j = 0; j < fieldSize - 2; j++) {
+            if (field[i][j] === symbol && field[i + 1][j + 1] === symbol && field[i + 2][j + 2] === symbol) {
+                winCells = [
+                    [i, j],
+                    [i + 1, j + 1],
+                    [i + 2, j + 2]
+                ];
             }
         }
 
     }
 
-    for (let i = 2; i < N; i++){
-        for (let j = 0; j < N - 2; j++){
-            if (field[i][j] === symbol && field[i-1][j+1] === symbol && field[i-2][j+2] === symbol){
-                winCells = [[i,j],[i-1,j+1],[i-2,j+2]];
+    for (let i = 2; i < fieldSize; i++) {
+        for (let j = 0; j < fieldSize - 2; j++) {
+            if (field[i][j] === symbol && field[i - 1][j + 1] === symbol && field[i - 2][j + 2] === symbol) {
+                winCells = [
+                    [i, j],
+                    [i - 1, j + 1],
+                    [i - 2, j + 2]
+                ];
             }
         }
 
     }
-    for (const cell of winCells){
+
+    for (const cell of winCells) {
         renderSymbolInCell(symbol, cell[0], cell[1], "#ff0000");
     }
-    return !!winCells;
+
+    return winCells.length != 0;
 }
 
 function finishGame(result) {
     if (result === CROSS || result == ZERO) {
         alert(result);
-    } else alert('Победила дружба!') 
+    } else alert('Победила дружба!')
     gameEnded = true;
 }
 
-function cellClickHandler (row, col) {
+function cellClickHandler(row, col) {
+    makeMove(row, col);
+}
+
+function makeMove(row, col) {
     if (gameEnded) return;
     console.log(`Clicked on cell: ${row}, ${col}`);
     if (field[row][col] != EMPTY) return;
     field[row][col] = currentSymbol;
     renderSymbolInCell(currentSymbol, row, col);
     currentSymbol = currentSymbol === ZERO ? CROSS : ZERO;
-    if (findWinLine(CROSS)){
+    if (findWinLine(CROSS)) {
         finishGame(CROSS);
-    } else if (findWinLine(ZERO)){
+    } else if (findWinLine(ZERO)) {
         finishGame(ZERO);
     } else if (checkNoMoreMoves()) {
         finishGame();
     }
+    console.log( document.getElementById('ai').value);
+    if (document.getElementById('ai').checked) {
+        if (!aiMoved) {
+            aiMoved = true;
+            for (let randomRow = 0; randomRow < fieldSize; ++randomRow) {
+                for (let randomCol = 0; randomCol < fieldSize; ++randomCol) {
+                    if (field[randomRow][randomCol] === EMPTY) {
+                        makeMove(randomRow, randomCol);
+                        return;
+                     }
+                }
+            }
+        } else aiMoved = false;
+    } 
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+function resetClickHandler() {
+    fieldSize = Number(document.getElementById('fieldSize').value) || 3;
+    if (fieldSize < 3 || fieldSize > 50) {
+        fieldSize = 3;
+    }
+    renderGrid(fieldSize);
+    console.log(fieldSize);
     console.log('reset!');
-    field = Array(N).fill().map(() => Array(N).fill(EMPTY));
-    for (let row = 0; row < N; ++row) {
-        for (let col = 0; col < N; ++col) {
+    field = Array(fieldSize).fill().map(() => Array(fieldSize).fill(EMPTY));
+    for (let row = 0; row < fieldSize; ++row) {
+        for (let col = 0; col < fieldSize; ++col) {
             renderSymbolInCell(EMPTY, row, col);
         }
     }
@@ -132,7 +175,7 @@ function resetClickHandler () {
 
 /* Test Function */
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -143,7 +186,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -156,6 +199,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
