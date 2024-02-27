7a410e306b25aebb4e343ff1973b9b63807a6cfa
ninimimm <nikitachernov89221156246@yandex.ru>
2024-02-27 22:44:37 +0500

    fix bug

--- index.js
+++ index.js
@@ -169,9 +169,9 @@ function addResetListener() {
 }
 
 function resetClickHandler() {
-    FIELD = []
-    PLAYER = true
-	STEPS = 0
+    FIELD = [];
+    PLAYER = true;
+	STEPS = 0;
     for (let i = 0; i < SIZE; i++){
         for (let j = 0; j < SIZE; j++){
             renderSymbolInCell(' ', i, j);
