f08b37541bb72408ca0ea933e291d383814c8caf
ricovaniy <115656372+ricovaniy@users.noreply.github.com>
2024-02-27 19:09:13 +0500

    task

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const container = document.getElementById('fieldWrapper');
 
 startGame();
 addResetListener();
-
+dim = 1;
 function startGame () {
-    renderGrid(3);
+    dim = prompt('Введите сколько на сколько будет поле', 3);
+    renderGrid(dim);
 }
 
 function renderGrid (dimension) {
@@ -25,17 +26,68 @@ function renderGrid (dimension) {
         container.appendChild(row);
     }
 }
-
+let lastClick = ZERO;
+let field = Array.from({length: dim* dim}, () => EMPTY);
+let fin = false;
 function cellClickHandler (row, col) {
-    // Пиши код тут
+    const index = (3 * row + col);
+    if(field[index] === EMPTY && !fin){
+        if(lastClick === ZERO){
+            renderSymbolInCell(CROSS, row, col);
+            lastClick = CROSS;
+        } else{
+            renderSymbolInCell(ZERO, row, col);
+            lastClick = ZERO;
+        }
+        field[index] = lastClick;
+    }
     console.log(`Clicked on cell: ${row}, ${col}`);
 
-
+    const winner = hasWinner();
+    console.log(winner);
+    if(winner !== EMPTY){
+        fin = true;
+        setTimeout(alert, 1,`Победил ${winner === ZERO ? 'Нолик' : 'Крестик'}`);
+    }
+    if(field.indexOf(EMPTY) === -1){
+        alert('Победила дружба!!!');
+    }
     /* Пользоваться методом для размещения символа в клетке так:
         renderSymbolInCell(ZERO, row, col);
      */
 }
 
+function hasWinner(){
+    for(let i = 0; i <= 2 ; i++){
+        if(field[i] === field[i + 3] && field[i] === field[i + 6]){
+            renderSymbolInCell(field[i], 0, i, '#f00')
+            renderSymbolInCell(field[i], 1, i, '#f00')
+            renderSymbolInCell(field[i], 2, i, '#f00')
+            return field[i];
+        }
+    }
+    for(let i = 0; i <= 6; i += 3) {
+        if (field[i] === field[i + 1] && field[i] === field[i + 2]) {
+            renderSymbolInCell(field[i], i, 0, '#f00')
+            renderSymbolInCell(field[i], i, 1, '#f00')
+            renderSymbolInCell(field[i], i, 2, '#f00')
+            return field[i];
+        }
+    }
+    if(field[0] === field[4] && field[0] === field[8]){
+        renderSymbolInCell(field[0], 0, 0, '#f00')
+        renderSymbolInCell(field[0], 1, 1, '#f00')
+        renderSymbolInCell(field[0], 2, 2, '#f00')
+        return field[0];
+    }
+    if(field[2] === field[4] && field[2] === field[6]){
+        renderSymbolInCell(field[2], 0, 2, '#f00')
+        renderSymbolInCell(field[2], 1, 1, '#f00')
+        renderSymbolInCell(field[2], 2, 0, '#f00')
+        return field[2];
+    }
+    return EMPTY;
+}
 function renderSymbolInCell (symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
@@ -54,7 +106,9 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
-    console.log('reset!');
+    field = Array.from({length: 9}, () => EMPTY);
+    fin = false;
+    renderGrid(3);
 }
 
 
